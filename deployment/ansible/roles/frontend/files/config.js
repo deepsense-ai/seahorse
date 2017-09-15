2e3aa89cdfae0bb2400bee4a8fa3c946a965c7e3
Jan Burka <jan.burka@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2620 Expose only necessary ports from vagrant image.
    Change-Id: If615ac8239762255f19588197cff499928510b85

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -1,12 +1,12 @@
 angular.module('ds.lab').constant('config', {
-  'apiHost': 'http://localhost',
+  'apiHost': 'http://172.28.128.100',
   'apiPort': '9080',
   'apiVersion': '1.0.0',
   'docsHost': 'https://seahorse.deepsense.io',
-  'notebookHost': 'http://localhost:8888',
+  'notebookHost': 'http://172.28.128.100:8888',
   'editorVersion': '${EDITOR_VERSION}',
   'urlApiVersion': 'v1',
   'resultsRefreshInterval': 10000,
-  'socketConnectionHost': 'http://localhost:15674/',
+  'socketConnectionHost': 'http://172.28.128.100:15674/',
   'socketReconnectionInterval': 1000
 });
