2906ab11c9b4d3057899b424caf7730f51c09572
Piotr Więckowski <piotr.wieckowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-3374 Disable debugInfo in Seahorse to improve performance
    Change-Id: Ib4e9c0945ac7fabaffaa67deb683d2aea8b0077e

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -8,5 +8,6 @@ angular.module('ds.lab').constant('config', {
   'urlApiVersion': 'v1',
   'resultsRefreshInterval': 10000,
   'socketConnectionHost': 'http://172.28.128.100:8000/',
-  'socketReconnectionInterval': 1000
+  'socketReconnectionInterval': 1000,
+  'debugInfoEnabled': false
 });
