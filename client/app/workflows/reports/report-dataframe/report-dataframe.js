bf6766d64079e9b5904883d97476f12067bf1300
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Fix bug about not showing distributions.  - ng-switch uses wrong call.  - Because report-dataframe uses fixed string names, hack it more to support "Column Names and Types" table  - Change 'categorical' with 'discrete'
    Change-Id: I09e21fd7419a928cc9379f974071b2f8b0d0ce30

--- client/app/workflows/reports/report-dataframe/report-dataframe.js
+++ client/app/workflows/reports/report-dataframe/report-dataframe.js
@@ -9,11 +9,25 @@ function ReportDataframe() {
     templateUrl: 'app/workflows/reports/report-dataframe/report-dataframe.html',
     replace: 'true',
     controller: function() {
+      // TODO Use some enum instead of name here.
       this.tableData = this.data['Data Sample'];
+      this.tableData = this.tableData ? this.tableData : this.data['Column Names and Types']
       this.tableSizes = this.data['DataFrame Size'];
       this.tableColumnsData = {};
       _.forEach(this.distributionsTypes, function(distType, colName) {
-        let icon = (distType === 'categorical') ? 'fa-pie-chart' : 'fa-bar-chart-o';
+        let icon = undefined;
+        switch (distType) {
+          case 'discrete':
+            icon = 'fa-pie-chart';
+            break;
+          case 'continuous':
+            icon = 'fa-bar-chart-o';
+            break
+          // TODO Handle 'no_distribution'
+          default:
+            console.log('Unknown distType: ' + distType + ". Falling back to bar chart");
+            icon = 'fa-bar-chart-o';
+        }
         this.tableColumnsData[colName] = {
           'icon': icon
         };
