b242827b89bff7978abee93e51ac09cf51a7cabe
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2203 Add workflow parameter definition.
    Change-Id: I840037b2f038e5594157c49f23e6191d6790c6da

--- src/common-parameters/common-parameter-factory.js
+++ src/common-parameters/common-parameter-factory.js
@@ -5,6 +5,7 @@ let NumericParameterConstructor = require('./common-numeric-parameter.js');
 let StringParameterConstructor = require('./common-string-parameter.js');
 let BooleanParameterConstructor = require('./common-boolean-parameter.js');
 let CodeSnippetParameterConstructor = require('./common-code-snippet-parameter.js');
+let WorkflowParameterConstructor = require('./common-workflow-parameter.js');
 let SingleChoiceParameterConstructor = require('./common-choice-parameter/common-single-choice-parameter.js');
 let MultipleChoiceParameterConstructor = require('./common-choice-parameter/common-multiple-choice-parameter.js');
 let SelectorParameterConstructor = require('./common-selector/common-selector-parameter.js');
@@ -21,6 +22,7 @@ let parameterConstructors = {
   'string': StringParameterConstructor,
   'boolean': BooleanParameterConstructor,
   'codeSnippet': CodeSnippetParameterConstructor,
+  'workflow': WorkflowParameterConstructor,
   'choice': SingleChoiceParameterConstructor,
   'multipleChoice': MultipleChoiceParameterConstructor,
   'selector': SelectorParameterConstructor,
@@ -100,12 +102,15 @@ let ParameterFactory = {
           if (!_.isUndefined(Constructor)) {
             parametersList.push(new Constructor(options, node, this));
           }
+        } else {
+          console.warn("No constructor for param of type " + paramSchema.type);
         }
       }
     }
 
     return new ParametersListConstructor({ parameters: parametersList });
   }
+
 };
 
 module.exports = ParameterFactory;
