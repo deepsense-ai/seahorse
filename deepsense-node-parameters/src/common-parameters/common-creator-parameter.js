5b02e360caf133b707497a6c516241e204ee3139
Piotr Więckowski <piotr.wieckowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2863 Add validation to SingleColumnCreator
    Change-Id: I776ae3593a9c91030a9a9a91b4b6f6b42856d938

--- deepsense-node-parameters/src/common-parameters/common-creator-parameter.js
+++ deepsense-node-parameters/src/common-parameters/common-creator-parameter.js
@@ -7,6 +7,7 @@
 'use strict';
 
 let GenericParameter = require('./common-generic-parameter.js');
+let ValidatorFactory = require('./common-validators/common-validator-factory.js');
 
 class CreatorParameter extends GenericParameter {
   constructor(options) {
@@ -14,11 +15,18 @@ class CreatorParameter extends GenericParameter {
     this.name = options.name;
     this.initValue(options.value, options.schema);
     this.schema = options.schema;
+    this.validator = ValidatorFactory.createValidator(this.schema.type, this.schema.validator);
   }
 
   serialize() {
     return this.value;
   }
+
+  validate() {
+    return this.validator.validate(this.getValueOrDefault());
+  };
+
 }
+
 
 module.exports = CreatorParameter;
