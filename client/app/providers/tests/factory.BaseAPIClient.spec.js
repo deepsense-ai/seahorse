1089fc1fd80968f0a13748d2d23f040aa49cea03
Piotr Zarowny <piotr.zarowny@codilime.com>
2017-09-15 00:00:00 +0000

    Add missing test for BaseAPIClient
    Change-Id: If006e2b22eb08462ce8ed5a87434f28ceabfaebd

--- client/app/providers/tests/factory.BaseAPIClient.spec.js
+++ client/app/providers/tests/factory.BaseAPIClient.spec.js
@@ -70,19 +70,50 @@ describe('BaseAPIClient', () => {
       let promise = client.makeRequest('GET', url);
       expect(promise).toEqual(jasmine.any(Object));
       expect(promise.then).toEqual(jasmine.any(Function));
+      expect(promise.catch).toEqual(jasmine.any(Function));
 
       $httpBackend.flush();
     });
 
-    it('which return promise & resolve on request success', () => {
+    it('which return promise & resolve it on request success', () => {
+      let success = false,
+          error   = false,
+          responseData;
+
       $httpBackend.expectGET(url);
 
       let promise = client.makeRequest('GET', url);
+      promise.then((data) => {
+        success = true;
+        responseData = data;
+      }).catch(() => {
+        error = true;
+      });
+
+      $httpBackend.flush();
+      expect(success).toBe(true);
+      expect(error).toBe(false);
+      expect(responseData).toEqual(response);
+    });
+
+    it('which return promise & rejects it on request error', () => {
+      let success = false,
+          error   = false;
+
+      $httpBackend.expectGET(url);
+
+      let promise = client.makeRequest('GET', url);
+      promise.then(() => {
+        success = true;
+      }).catch(() => {
+        error = true;
+      });
+
+      mockRequest.respond(500, 'Server Error');
       $httpBackend.flush();
 
-      promise.then((data) => {
-        expect(data).toEqual(response);
-      });
+      expect(success).toBe(false);
+      expect(error).toBe(true);
     });
 
   });
