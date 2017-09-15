a2864e8c1408b4886bbcf49edfeba1f0e992853f
Jaroslaw Osmanski <jaroslaw.osmanski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-750 Logout link on workflow and home pages
    Change-Id: I13d3ad594ebb21ab5d05321db08d1093199eb24c

--- app/server/auth/stub.js
+++ app/server/auth/stub.js
@@ -9,6 +9,8 @@ module.exports = {
 
 function init(app) {
   app.use(function(req, res, next) {
+    // user_id is also used in fronted for detecting if oauth is on.
+    // If user_id is 00000000-0000-0000-0000-000000000001 then auth is off.
     req.user = {
       user_id: "00000000-0000-0000-0000-000000000001",
       user_name: "user",
