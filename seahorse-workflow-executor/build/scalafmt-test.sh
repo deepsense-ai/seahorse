c2945e5b275c19fd5f1084246fef3b020dff6177
Michał Szostek <avquiraniel@gmail.com>
2017-09-15 00:00:00 +0000

    Remove obsolete "jenkins" directory
    * Remove obsolete "publish.sh" script
    * Rename "jenkins" to "build" in we

--- seahorse-workflow-executor/build/scalafmt-test.sh
+++ seahorse-workflow-executor/build/scalafmt-test.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd `dirname $0`"/../jenkins"
+cd `dirname $0`"/../build"
 ./scalafmt.sh --test
