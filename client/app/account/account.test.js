e08c301632e6dafd18282fe7c74a36785bc2d43a
Tadeusz Piskozub <tadeusz.piskozub@codilime.com>
2017-09-15 00:00:00 +0000

    Added tests for Account and Datasets.
    Change-Id: I6d91b7a974b3bb5e0463dbf4bee17a9da9cc6aee

---
+++ client/app/account/account.test.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015, CodiLime Inc.
+ */
+'use strict';
+
+describe('Account test', function() {
+  var mod;
+
+  mod = require('./account.module.js');
+
+  beforeEach(function() {
+    angular.mock.module('ui.router');
+    angular.mock.module('ds.account');
+  });
+
+  describe('dataset list', function() {
+    var scope, ctrl;
+    beforeEach(function() {
+      angular.mock.inject(function($controller, $rootScope) {
+        ctrl = $controller('Account');
+      });
+    });
+
+    it('should have label', function() {
+      expect(ctrl.label).toBe('Account info / settings page');
+    });
+  });
+});
---
+++ client/app/datasets/datasets.test.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015, CodiLime Inc.
+ */
+'use strict';
+
+describe('Datasets test', function() {
+  var mod;
+
+  mod = require('./datasets.module.js');
+
+  beforeEach(function() {
+    angular.mock.module('ui.router');
+    angular.mock.module('ds.datasets');
+  });
+
+  describe('dataset list', function() {
+    var scope, ctrl;
+    beforeEach(function() {
+      angular.mock.inject(function($controller, $rootScope) {
+        ctrl = $controller('DatasetList');
+      });
+    });
+
+    it('should say you do not have any datasets', function() {
+      expect(ctrl.setsLabel).toBe('You do not have any datasets!');
+    });
+  });
+});
