5e1aaeafd01e27bc0c620674d638a95d6886af21
Jacek Laskowski <jacek.laskowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-495 Register missing DOperations for Case 3: Demand Forecasting
    Change-Id: If4425f454eff71a84670a458164faa2fa94eba17

--- experimentmanager/src/main/scala/io/deepsense/experimentmanager/CatalogRecorder.scala
+++ experimentmanager/src/main/scala/io/deepsense/experimentmanager/CatalogRecorder.scala
@@ -1,7 +1,5 @@
 /**
  * Copyright (c) 2015, CodiLime, Inc.
- *
- * Owner: Rafal Hryciuk
  */
 
 package io.deepsense.experimentmanager
@@ -13,7 +11,8 @@ import io.deepsense.deeplang.doperables.dataframe.DataFrame
 import io.deepsense.deeplang.doperations._
 
 /**
- * Object used to register all desired DOperables and DOperations.
+ * Object used to register all desired [[io.deepsense.deeplang.DOperable]]s
+ * and [[io.deepsense.deeplang.DOperation]]s.
  */
 object CatalogRecorder {
 
@@ -25,45 +24,69 @@ object CatalogRecorder {
   }
 
   def registerDOperations(catalog: DOperationsCatalog) = {
-    catalog.registerDOperation[ReadDataFrame](
-      DOperationCategories.IO,
-      "Reads DataFrame from HDFS")
-
-    catalog.registerDOperation[WriteDataFrame](
-      DOperationCategories.IO,
-      "Writes DataFrame to HDFS")
-
-    catalog.registerDOperation[TimestampDecomposer](
-      DOperationCategories.Utils,
-      "Decomposes selected columns from timestamp to numeric")
-
-    catalog.registerDOperation[DataFrameSplitter](
-      DOperationCategories.Utils,
-      "Splits DataFrame into two DataFrames")
-
-    catalog.registerDOperation[CreateRidgeRegression](
-      DOperationCategories.ML.Regression,
-      "Creates untrained ridge regression model")
-
-    catalog.registerDOperation[TrainRegressor](
-      DOperationCategories.ML.Regression,
-      "Trains linear regression model")
-
-    catalog.registerDOperation[ScoreRegressor](
-      DOperationCategories.ML.Regression,
-      "Scores trained linear regression model")
-
-    catalog.registerDOperation[CrossValidateRegressor](
-      DOperationCategories.ML.Regression,
-      "Cross-validates regression model")
-
-    catalog.registerDOperation[OneHotEncoder](
-      DOperationCategories.Utils,
-      "One-hot encodes categorical columns")
-
     catalog.registerDOperation[FileToDataFrame](
       DOperationCategories.IO,
       "Converts a file to a DataFrame"
+    )
+
+    catalog.registerDOperation[ReadDataFrame](
+      DOperationCategories.IO,
+      "Reads DataFrame from HDFS"
+    )
+
+    catalog.registerDOperation[WriteDataFrame](
+      DOperationCategories.IO,
+      "Writes DataFrame to HDFS"
+    )
+
+    catalog.registerDOperation[DataFrameSplitter](
+      DOperationCategories.Utils,
+      "Splits DataFrame into two DataFrames"
+    )
+
+    catalog.registerDOperation[Join](
+      DOperationCategories.Utils,
+      "Joins two DataFrames to a DataFrame"
+    )
+
+    catalog.registerDOperation[OneHotEncoder](
+      DOperationCategories.Utils,
+      "One-hot encodes categorical columns"
+    )
+
+    catalog.registerDOperation[ProjectColumns](
+      DOperationCategories.Utils,
+      "Projects columns...FIXME"
+    )
+
+    catalog.registerDOperation[TimestampDecomposer](
+      DOperationCategories.Utils,
+      "Decomposes selected columns from timestamp to numeric"
+    )
+
+    catalog.registerDOperation[CreateRidgeRegression](
+      DOperationCategories.ML.Regression,
+      "Creates untrained ridge regression model"
+    )
+
+    catalog.registerDOperation[EvaluateRegression](
+      DOperationCategories.ML.Regression,
+      "Evaluates regression...FIXME"
+    )
+
+    catalog.registerDOperation[TrainRegressor](
+      DOperationCategories.ML.Regression,
+      "Trains linear regression model"
+    )
+
+    catalog.registerDOperation[ScoreRegressor](
+      DOperationCategories.ML.Regression,
+      "Scores trained linear regression model"
+    )
+
+    catalog.registerDOperation[CrossValidateRegressor](
+      DOperationCategories.ML.Regression,
+      "Cross-validates regression model"
     )
   }
 }
