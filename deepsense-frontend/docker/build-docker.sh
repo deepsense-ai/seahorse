59800aeb40cbf4cf97af53dedd5bac79d613db82
wojciech-kubik <wojciech.kubik@codilime.com>
2017-09-15 00:00:00 +0000

    NPT-151 Change Docker Registry address
    Due to office change, our tools need to be migrated to AWS cloud in order to
    minimize downtime.
    I deployd new docker registry and migrated all data for the old one.
    Changes needed to be made everywhere registry address were used.
    
    Change-Id: I7263065e3fc138739a2b8744c079b61d4bf18186

--- deepsense-frontend/docker/build-docker.sh
+++ deepsense-frontend/docker/build-docker.sh
@@ -6,7 +6,7 @@
 TIMESTAMP=`date +"%d%m%Y-%H%M%S"`
 COMMIT_HASH=`git rev-parse HEAD`
 NAME="deepsense-frontend"
-DOCKER_REGISTRY="docker-registry.intra.codilime.com"
+DOCKER_REGISTRY="docker-repo.deepsense.codilime.com"
 TAG_VERSION="$DOCKER_REGISTRY/tap/$NAME:tap-$TIMESTAMP-$COMMIT_HASH"
 TAG_LATEST="$DOCKER_REGISTRY/tap/$NAME:latest"
 
@@ -25,4 +25,3 @@ docker build -t "$TAG_VERSION" -t "$TAG_LATEST" .
 echo "Pushing docker to repository $DOCKER_REGISTRY"
 docker push $TAG_VERSION
 docker push $TAG_LATEST
-
