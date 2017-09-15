da4f764d097e69f1b4c0ce836787254bb9dbc93c
wojciech-kubik <wojciech.kubik@codilime.com>
2017-09-15 00:00:00 +0000

    DS-3584 Added common scripts for building and publishing docker images. Added support for Quay repo.
    Test and verify procedure for WM and SM:
    
    1. Run: sbt clean compile it:compile workflowmanager/docker:publish
    	sbt clean compile it:compile sessionmanager/docker:publish
    2. Run: cd deployment/docker && ./publish-local-docker.sh ../../sessionmanager/ deepsense-sessionmanager
    	cd deployment/docker && ./publish-local-docker.sh ../../workflowmanager/ deepsense-workflowmanager
    3. When builds are completed, verify if new images are present in the docker repositories (private and Quay).
    
    Test and verify procedure for Notebooks and RabbitMQ:
    
    1. Run: cd deployment/docker && ./build-local-docker.sh ../../remote_notebook/ deepsense-notebooks
    	cd deployment/docker && ./build-local-docker.sh ../ansible/roles/rabbitmq_dockerized/files/ deepsense-rabbitmq
    2. Run: cd deployment/docker && ./publish-local-docker.sh ../../remote_notebook/ deepsense-notebooks
    	cd deployment/docker && ./publish-local-docker.sh ../ansible/roles/rabbitmq_dockerized/files/ deepsense-rabbitmq
    3. When builds are completed, verify if new images are present in the docker repositories (private and Quay).
    
    Change-Id: Ie8ce8f6536cc41ed65e3b96cb8df043a7c14b1f4

---
+++ deployment/docker/build-local-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) 2016, CodiLime Inc.
+#
+# Builds docker image from given project path and name.
+#
+# Example usage: ./build-docker.sh ../../remote_notebook/ deepsense-notebooks
+
+# Check if number of parameters is correct
+if [ $# != 2 ]; then
+  echo ">>> Exactly two parameters must be provided."
+  exit 1;
+fi
+
+PROJECT_PATH=$1;
+PROJECT_NAME=$2
+TAG_LATEST="$PROJECT_NAME:latest"
+
+# Validate input parameters
+if [ ! -d $PROJECT_PATH ]; then
+  echo ">>> $PROJECT_PATH does not exist or is not a directory."
+  exit 2;
+fi
+
+if [ ! -f $PROJECT_PATH/Dockerfile ]; then
+  echo ">>> No Dockerfile in given location."
+  exit 2;
+fi
+
+cd $PROJECT_PATH
+
+# Build and tag docker image
+echo ">>> Building docker and tagging it as latest"
+docker build -t "$TAG_LATEST" .
+
--- workflowmanager/build.sbt
+++ workflowmanager/build.sbt
@@ -12,13 +12,6 @@ enablePlugins(JavaAppPackaging, GitVersioning, DeepsenseUniversalSettingsPlugin)
 
 // Docker related configuration
 
-lazy val dockerRegistryAddress = "docker-registry.intra.codilime.com"
-lazy val dockerNamespace = "tap"
-lazy val dockerRegistry = dockerRegistryAddress + "/" + dockerNamespace
-
 dockerBaseImage := "anapsix/alpine-java:jre8"
 dockerExposedPorts := Seq(9080)
-dockerRepository := Some(dockerRegistry)
 dockerUpdateLatest := true
-packageName in Docker := packageName.value
-version in Docker := "tap-" + version.value
