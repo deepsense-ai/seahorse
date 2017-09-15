8b5606f6172163dcf860a50b42c0d26caf986eaa
Jan Burka <jan.burka@codilime.com>
2017-09-15 00:00:00 +0000

    Get cluster IP from env
    Change-Id: I5d3161c803df710065b4f2f8bd96e9278516abca

--- sessionmanager/src/main/scala/io/deepsense/sessionmanager/service/HostAddressResolver.scala
+++ sessionmanager/src/main/scala/io/deepsense/sessionmanager/service/HostAddressResolver.scala
@@ -8,9 +8,23 @@ import java.net.{Inet6Address, InetAddress, NetworkInterface}
 
 import scala.util.Try
 
-object HostAddressResolver {
+import io.deepsense.commons.utils.Logging
+
+object HostAddressResolver extends Logging {
 
   def getHostAddress(): String = {
+    val address = if (System.getenv("MANAGED_BY") == "TAP") {
+      logger.info("Getting host address from environment")
+      getHostAddressFromEnv()
+    } else {
+      logger.info("Getting host address from network interface")
+      getHostAddressFromInterface()
+    }
+    logger.info(s"Host address: $address")
+    address
+  }
+
+  private def getHostAddressFromInterface(): String = {
     import collection.JavaConversions._
     Try {
       val interfaces = NetworkInterface.getNetworkInterfaces.toIterable
@@ -26,5 +40,12 @@ object HostAddressResolver {
         }
       }
     }.get.headOption.getOrElse(InetAddress.getByName("127.0.0.1")).getHostAddress
+  }
+
+  private def getHostAddressFromEnv(): String = {
+    val hostname = System.getenv("HOSTNAME")
+    val podId = hostname.toUpperCase.split("-").head
+    val clusterIp = System.getenv(s"${podId}_SERVICE_HOST")
+    clusterIp
   }
 }
