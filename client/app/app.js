a740e945d65375c0a0dd7aad756c8c832c0e2f36
Maciej Stankiewicz <tawezik@gmail.com>
2017-09-15 00:00:00 +0000

    SHR-899 Define generic UI elements
    - Define base less files
    - Define fonts
    
    Change-Id: I6eea8d77999840677ad48b40456f0981e78d3ae1

--- client/app/app.js
+++ client/app/app.js
@@ -6,6 +6,7 @@ let browserValidator = require('./browser.validator.js');
 
 // Resources
 import '../css/app.less';
+import 'LESS/app.less';
 
 // App
 import { CommonModule } from 'COMMON/common.module';
