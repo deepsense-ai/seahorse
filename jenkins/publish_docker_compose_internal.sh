b044df97a899018228903241d6e1258caa7570c8
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Use backend SHA for backend docker images in publish_docker-compose script
    Possible scenario was:
    1) Sync backend docker images without SEAHORSE_BUILD_TAG
    2) Run build-all jobs with SEAHORSE_BUILD_TAG
      a) dockers won't be built because images with GIT_HASH are already built
      b) dockers won't be pushed with SEAHORSE_BUILD_TAG
    3) docker-compose would reference non-existend SEAHORSE_BUILD_TAG docker images.
    
    This commit fixes this scenario by using GIT_SHA instead of SEAHORSE_BUILD_TAG
    for backend images
    
    Change-Id: I434c66c1e8dae80be4ba077356d92cd93b47bb4d

--- jenkins/publish_docker_compose_internal.sh
+++ jenkins/publish_docker_compose_internal.sh
@@ -9,7 +9,12 @@ set -ex
 
 cd `dirname $0`"/../"
 
+# TODO Get rid of SEAHORSE_BUILD_TAG or make it optional
+
 SEAHORSE_BUILD_TAG=$1
+
+FRONTEND_TAG=${SEAHORSE_BUILD_TAG}
+BACKEND_TAG=`git rev-parse HEAD`
 
 echo "This script assumes it is run from deepsense-backend directory"
 
@@ -19,7 +24,7 @@ DOCKER_COMPOSE_TMPL="deployment/docker-compose/docker-compose.tmpl.yml"
 ARTIFACT_NAME="docker-compose-internal.yml"
 rm -f $ARTIFACT_NAME
 
-deployment/docker-compose/prepare_docker-compose $DOCKER_COMPOSE_TMPL $ARTIFACT_NAME $SEAHORSE_BUILD_TAG $SEAHORSE_BUILD_TAG
+deployment/docker-compose/prepare_docker-compose $DOCKER_COMPOSE_TMPL $ARTIFACT_NAME $FRONTEND_TAG $BACKEND_TAG
 
 echo 'Sending $ARTIFACT_NAME to snapshot artifactory'
 source jenkins/publish_to_artifactory_function.sh
