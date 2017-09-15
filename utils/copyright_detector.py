a111cfadec33ab67b93395cd6ac60610bea1f4ae
Piotr Łusakowski <piotr.lusakowski@codilime.com>
2017-09-15 00:00:00 +0000

    Add bash support to copyright detector

--- utils/copyright_detector.py
+++ utils/copyright_detector.py
@@ -5,11 +5,17 @@ import re
 import time
 
 
+def detect_shebang(stripped_line):
+    return stripped_line.startswith("#!")
+
+
 def detect_comment_start(stripped_line):
     if stripped_line.startswith("/*"):
         return "/*"
     elif stripped_line.startswith("//"):
         return "//"
+    elif stripped_line.startswith("#") and not detect_shebang(stripped_line):
+        return "#"
     else:
         return None
 
@@ -19,11 +25,7 @@ def is_still_block_comment(stripped_line):
 
 
 def is_still_line_comment(stripped_line):
-    return stripped_line.startswith("//")
-
-
-def is_comment_end(stripped_line):
-    return stripped_line.endswith("*/")
+    return stripped_line.startswith("//") or stripped_line.startswith("#")
 
 
 def detect_comment_block(it):
@@ -32,12 +34,12 @@ def detect_comment_block(it):
     line_comment_started = 2
     comment_ended = 3
 
-    State = namedtuple("State", ['state_no', 'in_it', 'pre', 'out'])
+    State = namedtuple("State", ['state_no', 'in_it', 'shebang', 'pre', 'out'])
 
     it = iter(it)
 
     def process(state):
-        state_no, in_it, pre, out = state
+        state_no, in_it, shebang, pre, out = state
 
         assert state_no != comment_ended
 
@@ -45,31 +47,38 @@ def detect_comment_block(it):
             line = next(in_it)
             stripped_line = line.strip()
         except StopIteration:
-            return State(comment_ended, in_it, pre, out)
+            return State(comment_ended, in_it, shebang, pre, out)
 
         if state_no == start:
+            if detect_shebang(stripped_line):
+                return State(start, in_it, [line], pre, out)
+
             comment_type = detect_comment_start(stripped_line)
             if comment_type is None:
-                return State(start, in_it, pre + [line], out)
+                return State(start, in_it, shebang, pre + [line], out)
             elif comment_type == '/*':
                 return State(state_no=block_comment_started,
                              in_it=itertools.chain([line], in_it),
+                             shebang=shebang,
                              pre=pre,
                              out=out)
             else:
                 return State(state_no=line_comment_started,
                              in_it=in_it,
+                             shebang=shebang,
                              pre=pre,
                              out=out + [line])
         elif state_no == block_comment_started:
             if is_still_block_comment(stripped_line):
                 return State(state_no=state_no,
                              in_it=in_it,
+                             shebang=shebang,
                              pre=pre,
                              out=out + [line])
             else:
                 return State(state_no=comment_ended,
                              in_it=in_it,
+                             shebang=shebang,
                              pre=pre,
                              out=out + [line])
         else:
@@ -77,15 +86,17 @@ def detect_comment_block(it):
             if is_still_line_comment(stripped_line):
                 return State(state_no=state_no,
                              in_it=in_it,
+                             shebang=shebang,
                              pre=pre,
                              out=out + [line])
             else:
                 return State(state_no=comment_ended,
                              in_it=itertools.chain([line], in_it),
+                             shebang=shebang,
                              pre=pre,
                              out=out)
 
-    st = State(start, it, [], [])
+    st = State(start, it, [], [], [])
     while st.state_no != comment_ended:
         st = process(st)
 
@@ -170,7 +181,7 @@ def is_comment_a_copyright(comment_lines):
 def replace_copyright_with(it, replacement_fun, default_year=None):
     if default_year is None:
         default_year = time.gmtime().tm_year
-    _, in_it, pre, out = detect_comment_block(it)
+    _, in_it, shebang, pre, out = detect_comment_block(it)
 
     if out:
         copyright_year = is_comment_a_copyright(out)
@@ -181,39 +192,50 @@ def replace_copyright_with(it, replacement_fun, default_year=None):
             else:
                 year_to_insert = copyright_year
 
-            return itertools.chain(pre, replacement_fun(year_to_insert), in_it)
+            return itertools.chain(shebang, pre, replacement_fun(year_to_insert), in_it)
 
         else:
             return itertools.chain(
+                shebang,
                 replacement_fun(default_year),
                 pre,
                 out,
                 in_it)
     else:
         return itertools.chain(
+            shebang,
             replacement_fun(default_year),
             pre,
             in_it)
 
 
-def apache_license_lines(year):
-    return """/**
- * Copyright {year}, deepsense.ai
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+def apache_license_lines(year, begin, end, line_prefix):
+    b = '{}\n'.format(begin) if begin is not None else ''
+    e = '{}\n'.format(end) if end is not None else ''
 
-""".format(year=year).splitlines(True)
+    return """{begin}{line_prefix} Copyright {year}, deepsense.ai
+{line_prefix}
+{line_prefix} Licensed under the Apache License, Version 2.0 (the "License");
+{line_prefix} you may not use this file except in compliance with the License.
+{line_prefix} You may obtain a copy of the License at
+{line_prefix}
+{line_prefix}     http://www.apache.org/licenses/LICENSE-2.0
+{line_prefix}
+{line_prefix} Unless required by applicable law or agreed to in writing, software
+{line_prefix} distributed under the License is distributed on an "AS IS" BASIS,
+{line_prefix} WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+{line_prefix} See the License for the specific language governing permissions and
+{line_prefix} limitations under the License.
+{end}
+""".format(year=year, begin=b, end=e, line_prefix=line_prefix).splitlines(True)
+
+
+def apache_license_lines_c_style(year):
+    return apache_license_lines(year, '/**', '*/', '*')
+
+
+def apache_license_lines_bash_style(year):
+    return apache_license_lines(year, None, None, '#')
 
 
 def replace_copyright_in_file_with(
@@ -249,6 +271,9 @@ def find_scala_files_under_dir(dir):
 def find_js_files_under_dir(dir):
     return find_files(lambda filename: filename.endswith(".js"))(dir)
 
+def find_sh_files_under_dir(dir):
+    return find_files(lambda filename: filename.endswith(".sh"))(dir)
+
 
 def replace_copyright_in_js_files_under_dir_with(
         dir,
@@ -266,6 +291,18 @@ def replace_copyright_in_scala_files_under_dir_with(
         replacement_fun,
         default_year=None):
     for filename in find_scala_files_under_dir(dir):
+        replace_copyright_in_file_with(
+            filename,
+            replacement_fun,
+            default_year)
+
+# The function below doesn't work flawlessly
+#   - its results need to be reviewed manually
+def replace_copyright_in_sh_files_under_dir_with(
+        dir,
+        replacement_fun,
+        default_year=None):
+    for filename in find_sh_files_under_dir(dir):
         replace_copyright_in_file_with(
             filename,
             replacement_fun,
