5b15be39d70663505bae7b3e678fa94410726dc5
Konrad Szałwiński <konrad.szalwinski@gmail.com>
2017-09-15 00:00:00 +0000

    + Add minification of JS and CSS + Remove unnecessary plugins in gulpFile + Unify list of directories with other components
    Change-Id: I303ff762f5bdae2a922253939d731bf7cd477b43

--- gulpfile.js
+++ gulpfile.js
@@ -4,86 +4,69 @@
  * Created by Oleksandr Tserkovnyi on 08.06.15.
  */
 
-var path = require('path'),
-  del = require('del');
+var path = require('path');
+var del = require('del');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+var babel = require("gulp-babel");
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var templateCache = require('gulp-angular-templatecache');
+var minifyCss = require('gulp-minify-css');
 
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  runSequence = require('run-sequence'),
-  browserSync = require('browser-sync').create(),
-  babel = require("gulp-babel"),
-  concat = require('gulp-concat'),
-  less = require('gulp-less'),
-  templateCache = require('gulp-angular-templatecache');
+var config = require('./config.json');
 
-var config = require('./config.json'),
-  devMode = !!gutil.env.dev;
-
-gulp.task('clean', function() {
-  return del([config.dist + '*', config.temp + '*'], {force: true});
+gulp.task('clean', function () {
+    return del([config.dist + '*', config.temp + '*'], {force: true});
 });
 
-gulp.task('html', function() {
-  return gulp.src(config.src + '**/*.html')
-    .pipe(templateCache({
-      module: 'deepsense-catalogue-panel'
-    }))
-    .pipe(gulp.dest(config.temp));
+gulp.task('html', function () {
+    return gulp.src(config.src + '**/*.html')
+        .pipe(templateCache({
+            module: 'deepsense-catalogue-panel'
+        }))
+        .pipe(gulp.dest(config.temp));
 });
 
-gulp.task('es6', function() {
-  return gulp.src(config.src + '**/*.js')
-    .pipe(gulp.dest(config.temp));
+gulp.task('es6', function () {
+    return gulp.src(config.src + '**/*.js')
+        .pipe(gulp.dest(config.temp));
 });
 
 gulp.task('less', function () {
-  return gulp.src(config.src + '**/*.less')
-    .pipe(less({
-      // paths: Array of paths to be used for @import directives
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest(config.temp));
+    return gulp.src(config.src + '**/*.less')
+        .pipe(less({
+            // paths: Array of paths to be used for @import directives
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(minifyCss())
+        .pipe(gulp.dest(config.temp));
 });
 
 gulp.task('concat', function () {
-  gulp.src(config.temp + '**/*.js')
-    .pipe(concat(config.names.js))
-    .pipe(babel())
-    .pipe(gulp.dest(config.dist));
+    gulp.src(config.temp + '**/*.js')
+        .pipe(concat(config.names.js))
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(gulp.dest(config.dist));
 
-  return gulp.src(config.temp + '**/*.css')
-    .pipe(concat(config.names.css))
-    .pipe(gulp.dest(config.dist))
-    .pipe(browserSync.stream());
+    return gulp.src(config.temp + '**/*.css')
+        .pipe(concat(config.names.css))
+        .pipe(gulp.dest(config.dist))
 });
 
-gulp.task('serve', function() {
-
-  browserSync.init({
-      server: './'
-  });
-
-  gulp.watch(config.src + '**/*', ['build']);
-  gulp.watch(config.test + '**/*', ['build']);
-  gulp.watch([config.src + '**/*', config.test + '**/*']).on('change', function () {
-    // a bit hacky
-    setTimeout(function () {
-        browserSync.reload();
-    }, 450);
-  });
+gulp.task('serve', function () {
+    gulp.watch(config.src + '**/*', ['build']);
+    gulp.watch(config.test + '**/*', ['build']);
 });
 
 gulp.task('build', function () {
-  runSequence('clean', ['es6', 'less', 'html'], 'concat');
-
-  return gulp.src('package.json')
-    .pipe(gulp.dest(config.dist));
+    return runSequence('clean', ['es6', 'less', 'html'], 'concat');
 });
 
 gulp.task('start', function () {
-  if (devMode) {
     runSequence('build', 'serve');
-  }
 });
 
 gulp.task('default', ['build']);
