3edb03339224183c09055b11dff4f75e6fa48445
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    Read frontend version from file.
    Change-Id: I59123085d95714e0f985643a0c129812dd133fa7

--- deployment/ansible/roles/frontend/templates/run.sh
+++ deployment/ansible/roles/frontend/templates/run.sh
@@ -4,6 +4,8 @@
 #
 # Overrides config.js with content evaluated from environment variables
 
+export EDITOR_VERSION=`cat build/FULL_VERSION`
+
 eval "echo \"$(<config.js)\"" > /usr/share/nginx/html/config.js
 
 # Restart nginx after configuration change
