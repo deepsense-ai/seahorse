43842560bdb293993737bc88a02858785d21d49f
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2203 Remove never used context menu code.
    Change-Id: I4284bcad6fab45604f89c320ca7977e8d0386701

--- client/app/workflows/graph-panel/graph-panel-node/graph-panel-node.module.js
+++ client/app/workflows/graph-panel/graph-panel-node/graph-panel-node.module.js
@@ -1,9 +1,6 @@
 'use strict';
 
 exports.inject = function(module) {
-  require('./context-menu/report-options.service.js')
-    .inject(module);
-
   require('./graph-panel-node.js')
     .inject(module);
 };
