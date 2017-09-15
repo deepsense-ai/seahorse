9cd1d17a7aaed508f0ee1b91aa8ad3534047662d
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1487 support versioning in frontend
    Change-Id: I0e3686da9cd7715b5b29de6f663ed90699412455

--- publish.sh
+++ publish.sh
@@ -6,8 +6,34 @@
 # This script expects no external parameters.
 # Version is calculated from current git sha, current time and BASE_VERSION variable
 
-BASE_VERSION="0.2.0-frontend"
+function jsonValue() {
+  KEY=$1
+  num=$2
+  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+
+BASE_VERSION=`cat package.json | jsonValue version 1 | xargs`
+SNAPSHOT_REPOSITORY="deepsense-frontend-snapshot"
+RELEASE_REPOSITORY="deepsense-frontend-release"
 NPM_REGISTRY_URL="http://10.10.1.124:4873"
+
+function calculate_is_snapshot_version() {
+  if [[ "$BASE_VERSION" == *SNAPSHOT ]]
+  then
+    export IS_SNAPSHOT=true
+  else
+    export IS_SNAPSHOT=false
+  fi
+}
+
+function calculate_repository_url() {
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    export REPOSITORY_URL="$ARTIFACTORY_URL/$SNAPSHOT_REPOSITORY/io/deepsense/deepsense-frontend"
+  else
+    export REPOSITORY_URL="$ARTIFACTORY_URL/$RELEASE_REPOSITORY/io/deepsense/deepsense-frontend"
+  fi
+}
 
 function prepare_environment() {
   echo "** Preparing Environment **"
@@ -27,10 +53,15 @@ function build() {
 
 function calculate_full_version() {
   echo "** Calculating version **"
-  DATE=`date -u +%Y-%m-%d_%H-%M-%S`
-  GIT_SHA=`git rev-parse HEAD`
-  GIT_SHA_PREFIX=${GIT_SHA:0:7}
-  export FULL_VERSION="${BASE_VERSION}-${DATE}-${GIT_SHA_PREFIX}-SNAPSHOT"
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    DATE=`date -u +%Y-%m-%d_%H-%M-%S`
+    GIT_SHA=`git rev-parse HEAD`
+    GIT_SHA_PREFIX=${GIT_SHA:0:7}
+    export FULL_VERSION="${BASE_VERSION}-${DATE}-${GIT_SHA_PREFIX}"
+  else
+    export FULL_VERSION=${BASE_VERSION}
+  fi
 }
 
 function package() {
@@ -42,7 +73,7 @@ function package() {
 function publish() {
   artifactLocalName=$1
   artifactVersion=$2
-  artifactRemoteName="${artifactVersion}.zip"
+  url=$3
 
   echo "** INFO: Uploading $artifactLocalName to $ARTIFACTORY_URL/${artifactVersion} **"
   md5Value="`md5sum "${artifactLocalName}"`"
@@ -54,7 +85,29 @@ function publish() {
    -H "X-Checksum-Md5: $md5Value" \
    -H "X-Checksum-Sha1: $sha1Value" \
    -T "${artifactLocalName}" \
-   "${ARTIFACTORY_URL}/${artifactVersion}/${artifactRemoteName}"
+   "$url"
+}
+
+function publishLatest() {
+  artifactLocalName=$1
+  artifactVersion="frontend-latest"
+  artifactRemoteName="${artifactVersion}.zip"
+
+  publish $artifactLocalName $artifactVersion "${REPOSITORY_URL}/${artifactVersion}/${artifactRemoteName}"
+}
+
+function publishVersion() {
+  artifactLocalName=$1
+  artifactVersion=$2
+  artifactRemoteName="${artifactVersion}.zip"
+
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    url="${REPOSITORY_URL}/$BASE_VERSION/${artifactVersion}/${artifactRemoteName}"
+  else
+    url="${REPOSITORY_URL}/${artifactVersion}/${artifactRemoteName}"
+  fi
+  publish $artifactLocalName $artifactVersion $url
 }
 
 function clean() {
@@ -64,8 +117,10 @@ function clean() {
 
 prepare_environment
 build
+calculate_is_snapshot_version
 calculate_full_version
+calculate_repository_url
 package
-publish "${FULL_VERSION}.zip" "${FULL_VERSION}"
-publish "${FULL_VERSION}.zip" "frontend-latest"
+publishVersion "${FULL_VERSION}.zip" "${FULL_VERSION}"
+publishLatest "${FULL_VERSION}.zip"
 clean
