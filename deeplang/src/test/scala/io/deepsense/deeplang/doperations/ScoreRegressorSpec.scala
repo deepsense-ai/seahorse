8aa307c6c1a2583b7bc6cdd3438ac1069081bc9f
Grzegorz Chilkiewicz <grzegorz.chilkiewicz@codilime.com>
2017-09-15 00:00:00 +0000

    DS-360 Create ScoreRegressor DOperation
    Change-Id: Ia57b4b50a92635a8f7f609383aa1d14003d8c41f

---
+++ deeplang/src/test/scala/io/deepsense/deeplang/doperations/ScoreRegressorSpec.scala
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2015, CodiLime Inc.
+ *
+ * Owner: Grzegorz Chilkiewicz
+ */
+package io.deepsense.deeplang.doperations
+
+import org.mockito.Mockito.when
+import org.scalatest.mock.MockitoSugar
+
+import io.deepsense.deeplang._
+import io.deepsense.deeplang.doperables.Scorable
+import io.deepsense.deeplang.doperables.dataframe.DataFrame
+
+class ScoreRegressorSpec extends UnitSpec with MockitoSugar {
+
+  val scoreRegressor = new ScoreRegressor
+
+  "ScoreRegressor" should "score model on dataframe" in {
+    val scorableMock = new Scorable {
+      override val score = mock[DMethod1To1[None.type, DataFrame, DataFrame]]
+    }
+    val executionContextStub = mock[ExecutionContext]
+    val dataframeStub = mock[DataFrame]
+    val resultDataframeStub = mock[DataFrame]
+
+    when(scorableMock.score.apply(
+      executionContextStub)(None)(dataframeStub)).thenReturn(resultDataframeStub)
+
+    val result = scoreRegressor.execute(executionContextStub)(Vector(scorableMock, dataframeStub))
+
+    result shouldBe Vector(resultDataframeStub)
+  }
+}
