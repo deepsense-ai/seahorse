859b037ac905c25dfb76ccc44690ecb7b9c9caa7
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Add missing token in oauth stub
    Change-Id: I02df6907b42cc4d067411b563491abc2511a85df

--- proxy/app/server/auth/stub.js
+++ proxy/app/server/auth/stub.js
@@ -11,7 +11,8 @@ function init(app) {
   app.use(function(req, res, next) {
     req.user = {
       user_id: "00000000-0000-0000-0000-000000000001",
-      user_name: "user"
+      user_name: "user",
+      accessToken: "sometoken"
     };
     next();
   });
