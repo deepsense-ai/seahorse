0763564618bb699f9cc6ac67fd1f6c8d01016a67
Grzegorz Chilkiewicz <grzegorz.chilkiewicz@codilime.com>
2017-09-15 00:00:00 +0000

    Fix 'seahorse-vm.box already exists' problem
    Change-Id: I839e61a3496197b1e8b82e954c494de6e07d3a0e

--- jenkins/build_vagrant_with_docker.sh
+++ jenkins/build_vagrant_with_docker.sh
@@ -17,5 +17,6 @@ wget $COMPOSE_FILE
 # Create Vagrant box
 vagrant destroy -f $VAGRANT_BOX_NAME &>destroy.log
 vagrant up $VAGRANT_BOX_NAME &>install.log
+rm -f $PUBLISH_DIR/$VAGRANT_BOX_NAME.box
 vagrant package --output $PUBLISH_DIR/$VAGRANT_BOX_NAME.box &>package.log
 vagrant destroy -f $VAGRANT_BOX_NAME &>destroy.log
