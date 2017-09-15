a9052166576992a882af697becb7780637784c77
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    Support docs version parameter.
    Change-Id: Ic3d71f6cb659b8f3c7bcd2788365afb0e56fbd4d

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -1,8 +1,8 @@
 angular.module('ds.lab').constant('config', {
   'apiHost': 'http://172.28.128.100',
   'apiPort': '9080',
-  'apiVersion': '1.0.0',
-  'docsHost': 'https://seahorse.deepsense.io',
+  'apiVersion': '${API_VERSION}',
+  'docsHost': '${DOCS_ADDRESS}',
   'notebookHost': 'http://172.28.128.100:8888',
   'editorVersion': '${EDITOR_VERSION}',
   'urlApiVersion': 'v1',
