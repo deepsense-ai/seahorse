d354c1cd3991f03aae91cb93ee4b90053a2cf1b2
Piotr Więckowski <piotr.wieckowski@codilime.com>
2017-09-15 00:00:00 +0000

    UX and LESS fixes
      * Create less files in modals directories so that they contain their specific css rules
      * Use closing tag for components
      * Code cleaning
    
    Change-Id: I67af502a813bc58b08bd1880c51cd5f97a33aa28

--- client/components/datasources/datasources-toolbar/datasources-toolbar.component.js
+++ client/components/datasources/datasources-toolbar/datasources-toolbar.component.js
@@ -1,8 +1,16 @@
 'use strict';
 
 import DatasourcesToolbarTemplate from './datasources-toolbar.html';
+
+// Assets
 import './datasources-toolbar.less';
 import './modals/datasources-modals.less';
+import './modals/modal-footer/modal-footer.less';
+import './modals/external-file-modal/external-file-modal.less';
+import './modals/database-modal/database-modal.less';
+import './modals/google-spreadsheet-modal/google-spreadsheet-modal.less';
+import './modals/hdfs-modal/hdfs-modal.less';
+import './modals/library-modal/library-modal.less';
 
 const DatasourcesToolbarComponent = {
   bindings: {},
