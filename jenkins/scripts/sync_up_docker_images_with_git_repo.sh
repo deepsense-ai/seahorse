112246cdf449516f99de45d9a62ded03111fa621
Maciej Szwaja <maciej.szwaja@codilime.com>
2017-09-15 00:00:00 +0000

    Set GIT_SHA in sync_up_with_docker_images_with_git_repo.sh
    Change-Id: Ibd5c592e3b6dcaa7d0bc925b09be20ee522cdc92

--- jenkins/scripts/sync_up_docker_images_with_git_repo.sh
+++ jenkins/scripts/sync_up_docker_images_with_git_repo.sh
@@ -4,16 +4,10 @@
 # This script is idempotent and lazy. You can safely call it from other scripts to ensure dockers are built.
 cd `dirname $0`"/../../"
 
+GIT_SHA=`git rev-parse HEAD`
 # Without it SM dockers SbtGit.GitKeys.gitCurrentBranch will resolve to git hash instead of branch
 # and fail with $GIT_SHA-latest doesnt exists.
 # TODO Fix SM docker building process and get rid of SEAHORSE_BUILD_TAG here
-SEAHORSE_BUILD_TAG="${SEAHORSE_BUILD_TAG:-$GIT_SHA}" # SET if SEAHORSE_BUILD_TAG not set
-SEAHORSE_BUILD_TAG="${SEAHORSE_BUILD_TAG:-$BACKEND_TAG}" # second fallback
-
-if [[ -z "$SEAHORSE_BUILD_TAG" ]]; then
-   echo '[WARN] Unable to set SEAHORSE_BUILD_TAG, sessionmanager will probably not be built correctly!!!'
-fi
-
-export SEAHORSE_BUILD_TAG
+export SEAHORSE_BUILD_TAG="${SEAHORSE_BUILD_TAG:-$GIT_SHA}" # SET if SEAHORSE_BUILD_TAG not set
 
 ./jenkins/manage-docker.py --sync --publish --all
