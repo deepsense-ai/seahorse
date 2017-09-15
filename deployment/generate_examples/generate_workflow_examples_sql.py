f9ea6fe7fd9c1b7d024a65500bd2120fe043b80f
Maciej Szwaja <maciej.szwaja@codilime.com>
2017-09-15 00:00:00 +0000

    Create repeatable migration script for workflow examples on each build.
    To retain backwards compatibility with earlier versions of Seahorse I
    restored the old V2__insert_examples.sql script, so that flyway won't
    complain about checksums mismatch. However, the
    generate_workflow_examples_sql.py script will produce a repeatable
    (ie. not checksummed by flyway) migration that will overwrite the old
    examples with whatever is currently in the repository. Thus, the
    examples are no longer part of the flyway's database versioning
    scheme, with the sole exception of the V2 version, which is now carved
    in stone (unless we want to break the compatibility - in that case I'd
    probably remove the V2 script altogether and renumber the rest).
    
    Change-Id: Id91ed54bf25e37a72db1ca88fb0dfc23ccccb89c

--- deployment/generate_examples/generate_workflow_examples_sql.py
+++ deployment/generate_examples/generate_workflow_examples_sql.py
@@ -3,29 +3,53 @@
 import sys, json
 import os
 import uuid
-import time
+import subprocess
+
+EXAMPLES_NAMESPACE_UUID = uuid.UUID('dbfb66e9-a34e-455e-bd9c-157964f03674')
+
+USER_ID = '00000000-0000-0000-0000-000000000000'
+USER_NAME = 'seahorse'
+
+DELETE_WF = "DELETE FROM workflows WHERE owner_id = '{}';".format(USER_ID)
+SELECT_WF = "SELECT id FROM workflows WHERE owner_id = '{}'".format(USER_ID) # no semicolon!
+DELETE_NOTEBOOK = "DELETE FROM notebooks WHERE workflow_id in ({});".format(SELECT_WF)
 
 INSERT_WF_FORMAT = (
-  "INSERT INTO workflows (id, created, updated, deleted, owner_id, owner_name, workflow) "
-  "VALUES ('{}', '{}', '{}', False, '00000000-0000-0000-0000-000000000000', 'seahorse', '{}');"
+    "INSERT INTO workflows (id, created, updated, deleted, owner_id, owner_name, workflow) "
+    "VALUES ('{}', '{}', '{}', False, '{}', '{}', '{}');"
 )
+
 INSERT_NOTEBOOK_FORMAT = "INSERT INTO notebooks (workflow_id, node_id, notebook) VALUES ('{}', '{}', '{}');"
 
 def compact_json(content):
-  return json.dumps(content, separators=(',', ':')).replace('\'', '\'\'')
+    return json.dumps(content, separators=(',', ':')).replace('\'', '\'\'')
 
-created = int(time.time() * 1000)
+def jsonFiles(dir):
+    return (file for file in sorted(os.listdir(dir), reverse = True)
+            if file.endswith(".json"))
 
-for file in sorted(os.listdir("examples")):
-  if file.endswith(".json"):
-    with open("examples/" + file, "r") as f:
-      id = uuid.uuid4()
-      content = json.load(f)
-      compact = compact_json(content)
-      print INSERT_WF_FORMAT.format(id, created, created, compact)
-      notebooks = content['thirdPartyData']['notebooks']
-      for node_id, notebook_json in notebooks.iteritems():
-        compact_notebook = compact_json(notebook_json)
-        print INSERT_NOTEBOOK_FORMAT.format(id, uuid.UUID(node_id), compact_notebook)
-      created -= 1
+if __name__ == "__main__":
+    examplesDir = "examples"
 
+    print DELETE_NOTEBOOK
+    print DELETE_WF
+
+    created = 0
+    for file in jsonFiles(examplesDir):
+        examplesFile = "{}{}{}".format(examplesDir, os.sep, file)
+        with open(examplesFile, "r") as f:
+            id = uuid.uuid5(EXAMPLES_NAMESPACE_UUID, file)
+            content = json.load(f)
+            compact = compact_json(content)
+            print INSERT_WF_FORMAT.format(id, created, created, USER_ID, USER_NAME, compact)
+            notebooks = content['thirdPartyData']['notebooks']
+            for node_id, notebook_json in notebooks.iteritems():
+                compact_notebook = compact_json(notebook_json)
+                print INSERT_NOTEBOOK_FORMAT.format(id, uuid.UUID(node_id), compact_notebook)
+
+        # We increment 'created' to retain the sorted ordering in the
+        # frontend.  The frontend displays workflows with newest on
+        # top, hence the reversed flag in the jsonFiles generator
+        # expression, ie. we want the workflows that appear earlier in
+        # the alphabetically sorted list to have greater creation time.
+        created += 1
