f08121c7c9f440ce23c4fd5994911a6e2b198c72
Michał Szostek <michal.szostek@deepsense.io>
2017-09-15 00:00:00 +0000

    Clean whole project before running tests
    Change-Id: If218d299bcc742f65ade96494104ccaf3836b7e9

---
+++ jenkins/build_and_run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+# This is added here since sbt clean doesn't clean everything; in particular, it doesn't clean
+# project/target, so we delete all "target". For discussion, see
+# http://stackoverflow.com/questions/4483230/an-easy-way-to-get-rid-of-everything-generated-by-sbt
+# and
+# https://github.com/sbt/sbt/issues/896
+find . -name target -type d -exec rm -rf {} \; || true
+
+sbt clean scalastylebackend test ds-it
