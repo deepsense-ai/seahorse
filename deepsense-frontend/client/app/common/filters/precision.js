6dbbf02314c1b5e0e20bfb92f49f721500f6fda8
Aleksander Wilk <aleksander.wilk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-3260 trailing zeros bug in reports
    Change-Id: I8e6f30581c802993d5dcf6cf05caba7b98f7f468

--- deepsense-frontend/client/app/common/filters/precision.js
+++ deepsense-frontend/client/app/common/filters/precision.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function removeTrailingZeros (str) {
+  // if no decimal or scientific notation used, return unchanged
+  if (!_.includes(str, '.') || _.includes(str, 'e')) {
+    return str;
+  }
+  //  remove trailing zeros
+  str = str.replace(/0+$/, '');
+  // if number ends with decimal, remove it too
+  if (str.charAt(str.length-1) === '.') {
+    str = str.slice(0, -1);
+  }
+  return str;
+}
+
+
 function precision() {
   const DEFAULT_PRECISION = 4;
 
@@ -14,7 +29,7 @@ function precision() {
       digits = DEFAULT_PRECISION;
     }
 
-    return numVal.toPrecision(digits);
+    return removeTrailingZeros(numVal.toPrecision(digits));
   };
 }
 
