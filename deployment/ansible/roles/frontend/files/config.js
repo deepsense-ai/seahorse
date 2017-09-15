660504f3484d7163be0887b2138f77473fb2e806
Wojciech Jurczyk <wojciech.jurczyk@codilime.com>
2017-09-15 00:00:00 +0000

    Set port of SessionManager (proxied) in Frontend
    Change-Id: I4d38f21085f9f3d8538b9e3fc11b82de457c6f55

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -4,6 +4,7 @@ angular.module('ds.lab').constant('config', {
   'apiVersion': '${API_VERSION}',
   'docsHost': '${DOCS_ADDRESS}',
   'notebookHost': 'http://172.28.128.100:8000/jupyter',
+  'sessionApiPort': '8000',
   'editorVersion': '${EDITOR_VERSION}',
   'urlApiVersion': 'v1',
   'resultsRefreshInterval': 10000,
