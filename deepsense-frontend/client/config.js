92b8f789f749b454b6e2d3782e2666da217338cc
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Bump version to 1.1.0
    Change-Id: I24291a2f2c9fd8f3fa512a4e68d82cb0efe191c6

--- deepsense-frontend/client/config.js
+++ deepsense-frontend/client/config.js
@@ -9,7 +9,7 @@ angular.module('ds.lab').constant('config', {
   'notebookHost': 'http://localhost:8888',
   'apiPort': '9080',
   'apiVersion': '1.0.0',
-  'editorVersion': '1.0.0',
+  'editorVersion': '1.1.0',
   'urlApiVersion': 'v1',
   'resultsRefreshInterval': 10000,
   'socketConnectionHost': 'http://localhost:15674/',
