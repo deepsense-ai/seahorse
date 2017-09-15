d2f14cd1a6e74ae34cc26025393e30ada3543dac
Wojciech Jurczyk <wojciech.jurczyk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1141 Use Apache licence 2.0; update copyrights
    https://codilime.atlassian.net/browse/DS-1141
    
    Change-Id: I9f5a8570dab115c249b944f5e12c872401b67fda

--- deeplang/src/main/scala/io/deepsense/deeplang/doperations/ConvertType.scala
+++ deeplang/src/main/scala/io/deepsense/deeplang/doperations/ConvertType.scala
@@ -1,5 +1,17 @@
 /**
- * Copyright (c) 2015, CodiLime Inc.
+ * Copyright 2015, CodiLime Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.deepsense.deeplang.doperations
@@ -124,7 +136,7 @@ case class ConvertType() extends DOperation1To1[DataFrame, DataFrame] {
       inputDFMetadata,
       columnNamesToConvert,
       targetType)
-    (outputKnowledge, selectionsWarnings ++ InferenceWarnings(conversionWarningsList:_*))
+    (outputKnowledge, selectionsWarnings ++ InferenceWarnings(conversionWarningsList: _*))
   }
 
   private def convertDataFrameMetadata(
