3ab5d084caca6eeb9c440d6e083e5588f5c8e1ad
Jakub Sadłowski <jakub.sadlowski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-919 User can see updated look and feel of Op. Catalogue with better UX
    Change-Id: I5efdf4943c3b6a0d588328a55f70d7acc2c4f29f

--- client/app/workflows/operations-catalogue/operations-catalogue.service.js
+++ client/app/workflows/operations-catalogue/operations-catalogue.service.js
@@ -5,10 +5,20 @@ class OperationsCatalogueService {
     'ngInject';
 
     this.allOperations = Operations.getCatalog();
+
+    this.visibleLevel = 1;
   }
 
   getAllOperations() {
     return this.allOperations;
+  }
+
+  setVisibleCatalogueLevel(number) {
+    this.visibleLevel = number;
+  }
+
+  getVisibleCatalogueLevel() {
+    return this.visibleLevel;
   }
 }
 
--- client/app/workflows/operations-catalogue/operations-list/operations-list.component.js
+++ client/app/workflows/operations-catalogue/operations-list/operations-list.component.js
@@ -6,8 +6,15 @@ const TYPE_CATEGORY = 'category';
 const TYPE_ITEM = 'item';
 
 const ELEMENT_WIDTH = 200;
+const ELEMENT_WIDTH_SMALLER = 180;
 const ELEMENT_BORDER = 2;
-const ELEMENT_HEIGHT = 40;
+const CATEGORY_HEIGHT = 40;
+
+const ITEM_HEIGHT = 25;
+const ITEM_MARGIN_TOP = 10;
+const ITEM_MARGIN_BOTTOM = 5;
+
+const CATALOG_MAX_HEIGHT = 240;
 
 const MAX_VISIBLE_ELEMENTS = 6;
 
@@ -16,17 +23,56 @@ const DIRECTION_LEFT = 'left';
 
 const OperationsListComponent = {
   bindings: {
-    items: '<',
     containment: '<',
+    level: '<',
     direction: '<',
+    items: '<',
     selectOperation: '&'
   },
   controller: class OperationsListController {
-    constructor(CanvasService, $element) {
+    constructor(CanvasService, OperationsCatalogueService, $element, $scope) {
       'ngInject';
+
+      this.currentLevel = 1;
+      this.visibleLevel = 1;
+      this.activeCategoryIndex = null;
 
       this.$element = $element;
       this.CanvasService = CanvasService;
+      this.OperationsCatalogueService = OperationsCatalogueService;
+
+      $scope.$watch(() => OperationsCatalogueService.getVisibleCatalogueLevel(), (level) => {
+        this.visibleLevel = level;
+      });
+    }
+
+    $onChanges(change) {
+      if (change.items) {
+        this.selectedItems = null;
+        this.activeCategoryIndex = null;
+      }
+
+      if (change.level && change.level.currentValue) {
+        this.currentLevel = change.level.currentValue + 1;
+      }
+
+    }
+
+    $postLink() {
+      // Prevent canvas wheel zooming
+      $(this.$element[0]).on('wheel', (e) => {
+        e.stopPropagation();
+      });
+
+      // Mousedown is used to drag element around - and it prevents using scrollbar on FF
+      $(this.$element[0]).on('mousedown', (e) => {
+        e.stopPropagation();
+      });
+    }
+
+    $onDestroy() {
+      $(this.$element[0]).off();
+      this.OperationsCatalogueService.setVisibleCatalogueLevel(1);
     }
 
     getNextLevelStyle() {
@@ -36,47 +82,53 @@ const OperationsListComponent = {
       };
     }
 
-    calculateNextLevelPosition(event, nextLevelItems) {
+    calculateNextLevelPosition(event, catalogLength, itemsLength) {
       // Get only the container element from event
       const element = event.target;
 
       const elementBoundingRect = element.getBoundingClientRect();
       const containmentBoundingRect = this.containment[0].getBoundingClientRect();
 
+      const nextElementWidth = (this.currentLevel > 1) ? ELEMENT_WIDTH_SMALLER : ELEMENT_WIDTH;
+      const currentElementWidth = (this.currentLevel > 2) ? ELEMENT_WIDTH_SMALLER : ELEMENT_WIDTH;
+
       // Calculate bounds
       const containerBottom = containmentBoundingRect.bottom;
       const containerRight = containmentBoundingRect.right;
       const containerLeft = containmentBoundingRect.left;
 
-      //C alculate starting points for nextLevel menu
+      // Calculate starting points for nextLevel menu
       const elementTop = elementBoundingRect.top;
       const elementLeft = elementBoundingRect.left;
       const elementRight = elementBoundingRect.right;
 
       // Calculate size of nextLevel menu
-      const nextLevelVisibleElements = Math.min(nextLevelItems, MAX_VISIBLE_ELEMENTS);
-      const nextLevelHeight = nextLevelVisibleElements * ELEMENT_HEIGHT * this.CanvasService.scale;
-      const nextLevelWidth = ELEMENT_WIDTH * this.CanvasService.scale;
+      const nextLevelCategoryHeight = catalogLength * CATEGORY_HEIGHT;
+      const nextLevelItemsMargin = (itemsLength) ? ITEM_MARGIN_BOTTOM + ITEM_MARGIN_TOP : 0;
+      const nextLevelItemsHeight = itemsLength * ITEM_HEIGHT + nextLevelItemsMargin;
+      const nextLevelSize = Math.min((nextLevelCategoryHeight + nextLevelItemsHeight), CATALOG_MAX_HEIGHT);
+      const nextLevelHeight = nextLevelSize * this.CanvasService.scale;
+      const nextLevelWidth = nextElementWidth * this.CanvasService.scale;
 
       // Adjust initial starting position to the origin element position
       this.nextLevelTop = element.offsetTop - element.parentNode.scrollTop - ELEMENT_BORDER;
       // Move up, if next level menu wont fit below
       if (elementTop + nextLevelHeight > containerBottom) {
-        this.nextLevelTop -= (nextLevelVisibleElements - 1) * ELEMENT_HEIGHT;
+        this.nextLevelTop -= nextLevelSize - CATEGORY_HEIGHT;
       }
 
       if (this.direction === DIRECTION_LEFT) {
         if (elementLeft - nextLevelWidth < containerLeft) {
-          this.nextLevelLeft = ELEMENT_WIDTH - 2 * ELEMENT_BORDER;
+          this.nextLevelLeft = currentElementWidth - 2 * ELEMENT_BORDER;
           // Change direction for the next level menu to expand in the same direction
           this.direction = DIRECTION_RIGHT;
         } else {
-          this.nextLevelLeft = -ELEMENT_WIDTH;
+          this.nextLevelLeft = -nextElementWidth;
         }
       } else if (elementRight + nextLevelWidth < containerRight) {
-          this.nextLevelLeft = ELEMENT_WIDTH - 2 * ELEMENT_BORDER;
+          this.nextLevelLeft = currentElementWidth - 2 * ELEMENT_BORDER;
         } else {
-          this.nextLevelLeft = -ELEMENT_WIDTH;
+          this.nextLevelLeft = -nextElementWidth;
           // Change direction for the next level menu to expand in the same direction
           this.direction = DIRECTION_LEFT;
         }
@@ -100,29 +152,9 @@ const OperationsListComponent = {
         return Object.assign({}, item, {type: TYPE_ITEM});
       });
       this.selectedItems = [...catalog, ...items];
-      this.calculateNextLevelPosition(event, this.selectedItems.length);
-    }
-
-    $onChanges(change) {
-      if (change.items) {
-        this.selectedItems = null;
-      }
-    }
-
-    $postLink() {
-      // Prevent canvas wheel zooming
-      $(this.$element[0]).on('wheel', (e) => {
-        e.stopPropagation();
-      });
-
-      // Mousedown is used to drag element around - and it prevents using scrollbar on FF
-      $(this.$element[0]).on('mousedown', (e) => {
-        e.stopPropagation();
-      });
-    }
-
-    $destroy() {
-      $(this.$element[0]).off();
+      this.calculateNextLevelPosition(event, catalog.length, items.length);
+      this.OperationsCatalogueService.setVisibleCatalogueLevel(this.currentLevel + 1);
+      this.activeCategoryIndex = index;
     }
   },
   templateUrl: operationsListTemplate
