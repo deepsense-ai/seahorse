e784ce00c0094a2fa9e6be266e7892a6bec0ace2
Piotr Łusakowski <piotr.lusakowski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2083 Shut down on error in PyExecutor
    Change-Id: I08f2b539e403cd0c7ac746a4796c7bf474e3e281

--- python/pyexecutor/pyexecutor.py
+++ python/pyexecutor/pyexecutor.py
@@ -2,12 +2,13 @@
 
 import time
 
+from py4j.java_gateway import JavaGateway, GatewayClient, java_import
+from py4j.protocol import Py4JError
+
 from code_executor import CodeExecutor
 from gateway_resolver import GatewayResolver
-from py4j.java_gateway import JavaGateway, GatewayClient, java_import
 from pyspark import SparkContext, SQLContext, SparkConf
 
-# TODO: get the address from env, or hardcode a sensible one
 RABBIT_MQ_ADDRESS = ("localhost", 5672)
 
 
@@ -18,19 +19,30 @@ class PyExecutor(object):
     def run(self):
         gateway = self._initialize_gateway(
             gateway_address=self.gateway_resolver.get_gateway_address())
+        if not gateway:
+            print 'Failed to initialize java gateway'
+            return
 
         # noinspection PyProtectedMember
         callback_server_port = gateway._callback_server.server_socket.getsockname()[1]
-        gateway.entry_point.reportCallbackServerPort(callback_server_port)
-
         spark_context, sql_context = self._initialize_spark_contexts(gateway)
-
         code_executor = CodeExecutor(spark_context, sql_context, gateway.entry_point)
-        gateway.entry_point.registerCodeExecutor(code_executor)
+
+        try:
+            gateway.entry_point.reportCallbackServerPort(callback_server_port)
+            gateway.entry_point.registerCodeExecutor(code_executor)
+        except Py4JError:
+            print 'Exception while registering codeExecutor, or callback server port'
+            gateway.close()
+            return
 
         # Wait for the end of the world
-        while True:
-            time.sleep(1)
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print 'Exiting on user\'s request'
+            gateway.close()
 
     @staticmethod
     def _initialize_spark_contexts(gateway):
@@ -53,16 +65,20 @@ class PyExecutor(object):
                               python_proxy_port=0,
                               start_callback_server=True,
                               auto_convert=True)
-
-        java_import(gateway.jvm, "org.apache.spark.SparkEnv")
-        java_import(gateway.jvm, "org.apache.spark.SparkConf")
-        java_import(gateway.jvm, "org.apache.spark.api.java.*")
-        java_import(gateway.jvm, "org.apache.spark.api.python.*")
-        java_import(gateway.jvm, "org.apache.spark.mllib.api.python.*")
-        java_import(gateway.jvm, "org.apache.spark.sql.*")
-        java_import(gateway.jvm, "org.apache.spark.sql.hive.*")
-        java_import(gateway.jvm, "scala.Tuple2")
-        java_import(gateway.jvm, "scala.collection.immutable.List")
+        try:
+            java_import(gateway.jvm, "org.apache.spark.SparkEnv")
+            java_import(gateway.jvm, "org.apache.spark.SparkConf")
+            java_import(gateway.jvm, "org.apache.spark.api.java.*")
+            java_import(gateway.jvm, "org.apache.spark.api.python.*")
+            java_import(gateway.jvm, "org.apache.spark.mllib.api.python.*")
+            java_import(gateway.jvm, "org.apache.spark.sql.*")
+            java_import(gateway.jvm, "org.apache.spark.sql.hive.*")
+            java_import(gateway.jvm, "scala.Tuple2")
+            java_import(gateway.jvm, "scala.collection.immutable.List")
+        except Py4JError:
+            print 'Error while initializing java gateway'
+            gateway.close()
+            return None
 
         return gateway
 
