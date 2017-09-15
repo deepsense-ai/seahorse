bd97bcf69d5cb1b6d5248b5f27f72607f30c375c
Jakub Sadłowski <jakub.sadlowski@codilime.com>
2017-09-15 00:00:00 +0000

    Remove workaround from documentation URL creation
    Change-Id: I7223fc58df87b627b992eb61380231055074daef

--- deepsense-frontend/client/app/version.factory.js
+++ deepsense-frontend/client/app/version.factory.js
@@ -4,9 +4,7 @@
 function VersionFactory(config) {
   let service = {};
   service.getDocsVersion = function() {
-    // TODO Workaround for lacking of documentation 1.2
-    return "1.1";
-    //return config.apiVersion.split('.').slice(0, 2).join('.');
+    return config.apiVersion.split('.').slice(0, 2).join('.');
   };
   service.editorVersion = config.editorVersion;
   return service;
