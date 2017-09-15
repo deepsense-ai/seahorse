85f9ece726e28d0d4b148e773728bb0a632e1116
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Add DKnowledge equality and hashCode.
    Change-Id: I6f00af301fc7115d455d318cbb53806179d2c93f

--- GraphLibrary/src/main/scala/io/deepsense/deeplang/DKnowledge.scala
+++ GraphLibrary/src/main/scala/io/deepsense/deeplang/DKnowledge.scala
@@ -10,10 +10,23 @@ package io.deepsense.deeplang
   */
 class DKnowledge[T <: DOperable](val types: Set[T]) {
   def this(args: T*) = this(Set[T](args: _*))
+
+  override def equals(other: Any): Boolean = {
+    other match {
+      case that: DKnowledge[_] => types == that.types
+      case _ => false
+    }
+  }
+
+  override def hashCode(): Int = types.hashCode()
 }
 
 object DKnowledge {
+  def apply[T <: DOperable](args: T*) = new DKnowledge[T](args: _*)
+
+  def apply[T <: DOperable](types: Set[T]) = new DKnowledge[T](types)
+
   implicit def asCovariant[T <: DOperable, U <: T](dk: DKnowledge[U]): DKnowledge[T] = {
-    new DKnowledge(dk.types.asInstanceOf[Set[T]])
+    DKnowledge(dk.types.asInstanceOf[Set[T]])
   }
 }
---
+++ GraphLibrary/src/test/scala/io/deepsense/deeplang/DKnowledgeSuite.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015, CodiLime, Inc.
+ *
+ * Owner: Witold Jedrzejewski
+ */
+
+package io.deepsense.deeplang
+
+import org.scalatest.FunSuite
+
+/** */
+class DKnowledgeSuite extends FunSuite {
+
+  test("DKnowledge[DOperable] with same content are equal") {
+    case class A(i: Int) extends DOperable
+    case class B(i: Int) extends DOperable
+
+    val knowledge1 = DKnowledge(A(1), B(2), A(3))
+    val knowledge2 = DKnowledge(A(1), A(3), B(2), A(1))
+    assert(knowledge1 == knowledge2)
+  }
+
+  test("DKnowledge[_] objects with same content are equal") {
+    def isAOrB(any: Any) = any.isInstanceOf[A] || any.isInstanceOf[B]
+    class A extends DOperable {
+      override def equals(any: Any) = isAOrB(any)
+    }
+    class B extends DOperable {
+      override def equals(any: Any) = isAOrB(any)
+    }
+
+    val knowledge1: DKnowledge[A] = DKnowledge(new A)
+    val knowledge2: DKnowledge[B] = DKnowledge(new B)
+    assert(knowledge1 == knowledge2)
+  }
+
+  test("DKnowledge with different content are not equal") {
+    case class A(i: Int) extends DOperable
+
+    val knowledge1 = DKnowledge(A(1))
+    val knowledge2 = DKnowledge(A(2))
+    assert(knowledge1 != knowledge2)
+  }
+}
