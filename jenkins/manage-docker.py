b5fd0b88de1903658de405ea6605ea53e533b65a
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Change all headers according to new scheme

--- jenkins/manage-docker.py
+++ jenkins/manage-docker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # PYTHON_ARGCOMPLETE_OK
-# Copyright 2017, deepsense.ai
+# Copyright 2017 deepsense.ai (CodiLime, Inc)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2015, deepsense.ai
+# Copyright 2015 deepsense.ai (CodiLime, Inc)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
