5391215341d0069a8bd63fda04fa9ecd98a94d4e
Jan Burka <jan.burka@codilime.com>
2017-09-15 00:00:00 +0000

    DS-3585 Parameterize WM address in jupyter
    Change-Id: I768a10dbd4e585b1c9a4b1dbcea3ecb4b0c38eb7

--- remote_notebook/jupyter_notebook_config.py
+++ remote_notebook/jupyter_notebook_config.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2015, CodiLime Inc.
 
+import os
+
 from wmcontents import WMContentsManager
 
 c = get_config()
@@ -15,8 +17,12 @@ c.NotebookApp.tornado_settings = {
     }
 }
 
+workflow_manager_url = "http://localhost:9080"
+if 'WM_URL' in os.environ:
+    workflow_manager_url = os.environ['WM_URL']
+
 c.NotebookApp.contents_manager_class = WMContentsManager
-c.WMContentsManager.workflow_manager_url = "http://localhost:9080"
+c.WMContentsManager.workflow_manager_url = workflow_manager_url
 c.WMContentsManager.kernel_name = "pyspark"
 c.WMContentsManager.kernel_display_name = "Python (Spark)"
 c.WMContentsManager.kernel_python_version = "2.7.10"
