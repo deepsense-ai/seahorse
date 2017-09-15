07f8d1992fc6cf0bbb951d63f5799dd2372ae3d5
Dominik Jastrzębski <dominik.jastrzebski@codilime.com>
2017-09-15 00:00:00 +0000

    Add docsHost field in config.js of frontend deployment
    Change-Id: I0b5fc83382e436328d9326f628cc459555d0a1b9

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -2,6 +2,7 @@ angular.module('ds.lab').constant('config', {
   'apiHost': '${PUBLIC_WM_ADDRESS}',
   'apiPort': '${PUBLIC_WM_PORT}',
   'apiVersion': '${API_VERSION}',
+  'docsHost': '${PUBLIC_DOCS_ADDRESS}',
   'editorVersion': '${EDITOR_VERSION}',
   'urlApiVersion': 'v1',
   'resultsRefreshInterval': '10000'
