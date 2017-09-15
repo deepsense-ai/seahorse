3317026c4cd45a23623b71347121dba16fa81161
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Improve title of DeepLangException
    Change-Id: If7964e60ee0e59ea26becd952573a54e8c7acde3

--- deeplang/src/main/scala/io/deepsense/deeplang/exceptions/DeepLangException.scala
+++ deeplang/src/main/scala/io/deepsense/deeplang/exceptions/DeepLangException.scala
@@ -23,6 +23,6 @@ abstract class DeepLangException(
     cause: Throwable = null)
   extends DeepSenseException(
     FailureCode.NodeFailure,
-    getClass.getSimpleName,
+    "DeepLang Exception",
     message,
     Option(cause))
