5d99e96146b991e7cdc8a0fa7a0324a28e6f91cd
Grzegorz Chilkiewicz <grzegorz.chilkiewicz@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2357 Make noninteractive mode default
    Change-Id: I98ccb952ab326248ceac1c76e11a25fb8a99b185

--- system_tests/lib/WorkflowExecutorClient.py
+++ system_tests/lib/WorkflowExecutorClient.py
@@ -59,7 +59,7 @@ class WorkflowExecutorClient(object):
 def spark_submit_command(**kwargs):
   return """spark-submit --class {we_class} --master {spark_master} --files {workflow_filename}\\
     --driver-class-path {jar} {spark_submit_options} {jar}\\
-    --noninteractive-mode --python-executor-path ./python/pyexecutor/pyexecutor.py\\
+    --python-executor-path ./python/pyexecutor/pyexecutor.py\\
     --workflow-filename {workflow_filename} --output-directory {output_dir}\\
     """.format(
     we_class=kwargs['we_class'],
