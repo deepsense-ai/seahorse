b315b156454ea4b02c44a4ad73b54d164a0ed325
Piotr Zarowny <piotr.zarowny@codilime.com>
2017-09-15 00:00:00 +0000

    DS-501 EntitiesAPIClinet: getReport method
    Change-Id: I393fb28f0a7fa3b219cd32580d5dbe2f340fbc89

--- client/app/providers/EntitiesAPIClient.factory.js
+++ client/app/providers/EntitiesAPIClient.factory.js
@@ -8,11 +8,25 @@
 
 /* @ngInject */
 function EntitiesAPIClientFactory(BaseAPIClient) {
+  const PATH_ENTITIES = '/entities';
+  const PATH_RAPORT = '/raport';
+
   function EntitiesAPIClient() {
     BaseAPIClient.call(this);
   }
   EntitiesAPIClient.prototype = Object.create(BaseAPIClient.prototype);
   EntitiesAPIClient.prototype.constructor = EntitiesAPIClient;
+
+  /**
+   * Returns entity report data.
+   *
+   * @param {string} id
+   *
+   * @return {Promise}
+   */
+  EntitiesAPIClient.prototype.getReport = function getReport(id) {
+    return this.makeRequest(this.METHOD_GET, this.API_PATH + PATH_ENTITIES + '/' + id + PATH_RAPORT);
+  };
 
   return new EntitiesAPIClient();
 }
