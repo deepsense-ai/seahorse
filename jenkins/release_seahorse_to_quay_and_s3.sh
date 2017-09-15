df7e72b35687326bd7558c70f3e48c35f84049b0
Michał Szostek <michal.szostek@deepsense.io>
2017-09-15 00:00:00 +0000

    Use Spark 2.1.1 workflowexecutor in publish
    Change-Id: Icfdeef832116b9127c3c8d1417a023cf8954f5a2

--- jenkins/release_seahorse_to_quay_and_s3.sh
+++ jenkins/release_seahorse_to_quay_and_s3.sh
@@ -46,8 +46,8 @@ echo "Publish to S3 workflowexecutor uber-jars"
 rm -f workflowexecutor.jar
 
 wget --output-document=workflowexecutor.jar \
-  "${ARTIFACTORY_URL}/${SEAHORSE_DISTRIBUTION_REPOSITORY}/io/deepsense/${SEAHORSE_BUILD_TAG}/workflowexecutor/workflowexecutor-2.11-2.0.2.jar"
-aws s3 cp workflowexecutor.jar s3://${WE_RELEASE_PATH}/workflowexecutor_2.11-${API_VERSION}.jar --acl public-read
+  "${ARTIFACTORY_URL}/${SEAHORSE_DISTRIBUTION_REPOSITORY}/io/deepsense/${SEAHORSE_BUILD_TAG}/workflowexecutor/workflowexecutor-2.11-2.1.1.jar"
+aws s3 cp workflowexecutor.jar s3://${WE_RELEASE_PATH}/workflowexecutor_2.11-${API_VERSION}-2.1.1.jar --acl public-read
 
 
 DOCKER_COMPOSE_YML="docker-compose.yml"
