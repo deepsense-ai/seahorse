acd210aef7ec106dd53c5b20ab2e999e6558d9e4
Jan Burka <jan.burka@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2482 Add NumPy library to vagrant image.
    Change-Id: If77e548e2ad0645648e1bfac8a5495ba4ed56cf1

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -2,7 +2,7 @@ angular.module('ds.lab').constant('config', {
   'apiHost': 'http://localhost',
   'apiPort': '9080',
   'apiVersion': '1.0.0',
-  'docsHost': 'https://seahorse.deepsense.io/',
+  'docsHost': 'https://seahorse.deepsense.io',
   'notebookHost': 'http://localhost:8888',
   'editorVersion': '${EDITOR_VERSION}',
   'urlApiVersion': 'v1',
