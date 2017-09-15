f3f2a0e9287a37a619013891f5b3ca11566385b2
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2749 Insert notebooks for usecases.
    Change-Id: Id650cd6caaa113376e4f2a381f37c4dcae6e09fe

--- deployment/ansible/roles/generate_examples/files/generate_examples.py
+++ deployment/ansible/roles/generate_examples/files/generate_examples.py
@@ -5,7 +5,11 @@ import os
 import uuid
 import time
 
-INSERT_FORMAT = "INSERT INTO workflows (id, created, updated, deleted, workflow) VALUES ('{}', '{}', '{}', False, '{}');"
+INSERT_WF_FORMAT = "INSERT INTO workflows (id, created, updated, deleted, workflow) VALUES ('{}', '{}', '{}', False, '{}');"
+INSERT_NOTEBOOK_FORMAT = "INSERT INTO notebooks (workflow_id, node_id, notebook) VALUES ('{}', '{}', '{}');"
+
+def compact_json(content):
+  return json.dumps(content, separators=(',', ':')).replace('\'', '\'\'')
 
 created = int(time.time() * 1000)
 
@@ -14,5 +18,10 @@ for file in sorted(os.listdir("examples")):
     with open("examples/" + file, "r") as f:
       id = uuid.uuid4()
       content = json.load(f)
-      compact = json.dumps(content, separators=(',', ':')).replace('\'', '\'\'')
-      print INSERT_FORMAT.format(id, created, created, compact)
+      compact = compact_json(content)
+      print INSERT_WF_FORMAT.format(id, created, created, compact)
+      notebooks = content['thirdPartyData']['notebooks']
+      for node_id, notebook_json in notebooks.iteritems():
+        compact_notebook = compact_json(notebook_json)
+        print INSERT_NOTEBOOK_FORMAT.format(id, uuid.UUID(node_id), compact_notebook)
+
