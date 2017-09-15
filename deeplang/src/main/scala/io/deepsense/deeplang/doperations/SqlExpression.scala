3a96f527990268f6079f273b9e239ef827004525
Dominik Jastrzębski <dominik.jastrzebski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1972 Update SQLExpression using new Params Lib
    Change-Id: Ie2fcc9963bb7279dbbe3f76cbc52a3931780eff8

--- deeplang/src/main/scala/io/deepsense/deeplang/doperations/SqlExpression.scala
+++ deeplang/src/main/scala/io/deepsense/deeplang/doperations/SqlExpression.scala
@@ -21,63 +21,51 @@ import scala.reflect.runtime.{universe => ru}
 import io.deepsense.deeplang.DOperation.Id
 import io.deepsense.deeplang.doperables.dataframe.{DataFrame, DataFrameBuilder}
 import io.deepsense.deeplang.parameters.{AcceptAllRegexValidator, ParametersSchema, StringParameter}
+import io.deepsense.deeplang.params.{StringParam, Params}
 import io.deepsense.deeplang.{DOperation1To1, ExecutionContext}
 
-case class SqlExpression() extends DOperation1To1[DataFrame, DataFrame] with OldOperation {
+case class SqlExpression() extends DOperation1To1[DataFrame, DataFrame] with Params {
   @transient
   override lazy val tTagTO_0 = ru.typeTag[DataFrame]
   @transient
   override lazy val tTagTI_0 = ru.typeTag[DataFrame]
 
-  val dataFrameIdParameter = StringParameter(
-    "An identifier that can be used in the SQL expression to refer to the input " +
-      "DataFrame. The value has to be unique in the workflow.",
-    None,
-    new AcceptAllRegexValidator(),
-    _value = None
-  )
+  val dataFrameId = StringParam(
+    name = "dataframe id",
+    description = "An identifier that can be used in the SQL expression to refer to the input " +
+      "DataFrame. The value has to be unique in the workflow.")
 
-  val expressionParameter = StringParameter(
-    "SQL Expression to be executed on the DataFrame",
-    None,
-    new AcceptAllRegexValidator(),
-    _value = None)
+  def getDataFrameId: String = $(dataFrameId)
+  def setDataFrameId(value: String): this.type = set(dataFrameId, value)
 
-  override val parameters: ParametersSchema = ParametersSchema(
-    SqlExpression.dataFrameIdParameterName -> dataFrameIdParameter,
-    SqlExpression.expressionParameterName -> expressionParameter
-  )
+  val expression = StringParam(
+    name = "expression",
+    description = "SQL Expression to be executed on the DataFrame")
+
+  def getExpression: String = $(expression)
+  def setExpression(value: String): this.type = set(expression, value)
+
   override val name: String = "SQL Expression"
   override val id: Id = "530e1420-7fbe-416b-b685-6c1e0f1137fc"
 
   override protected def _execute(context: ExecutionContext)(t0: DataFrame): DataFrame = {
-    logger.debug(s"SqlExpression(expression = '${expressionParameter.value}'," +
-      s" dataFrameId = '${dataFrameIdParameter.value}')")
-    t0.sparkDataFrame.registerTempTable(dataFrameId)
+    logger.debug(s"SqlExpression(expression = '${getExpression}'," +
+      s" dataFrameId = '${getDataFrameId}')")
+    t0.sparkDataFrame.registerTempTable(getDataFrameId)
     try {
       logger.debug(s"Table '$dataFrameId' registered. Executing the expression")
-      val sqlResult = context.sqlContext.sql(expression)
+      val sqlResult = context.sqlContext.sql(getExpression)
       DataFrameBuilder(context.sqlContext)
         .buildDataFrame(sqlResult)
     } finally {
-      logger.debug("Unregistering the temporary table" + dataFrameIdParameter.value)
-      context.sqlContext.dropTempTable(dataFrameId)
+      logger.debug("Unregistering the temporary table" + getDataFrameId)
+      context.sqlContext.dropTempTable(getDataFrameId)
     }
   }
-
-  def dataFrameId: String = dataFrameIdParameter.value
-  def expression: String = expressionParameter.value
 }
 
 object SqlExpression {
-  val expressionParameterName = "expression"
-  val dataFrameIdParameterName = "dataframe id"
-
-  def apply(sqlExpression: String, dataFrameId: String): SqlExpression = {
-    val operation = SqlExpression()
-    operation.expressionParameter.value = sqlExpression
-    operation.dataFrameIdParameter.value = dataFrameId
-    operation
-  }
+  def apply(sqlExpression: String, dataFrameId: String): SqlExpression =
+    new SqlExpression().setExpression(sqlExpression).setDataFrameId(dataFrameId)
 }
 
