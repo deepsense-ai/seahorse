69ff928fdc92360a8ad533907f62338acbbe10b7
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Merge all projects into one repository. Step 1
    * Move frontend to child catalog
    * Move navigation project into super-repo and change package.json
    
    Change-Id: Ie76c98b2b5ce6b9b5b806b14fb0fc97589aab047

---
+++ deepsense-navigation-panel/src/deepsense-navigation-panel.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2015, CodiLime Inc.
+ *
+ * Created by: Grzegorz Swatowski
+ */
+
+'use strict';
+
+angular.module('deepsense.navigation-panel', [
+  'deepsense.navigation-panel.partials',
+  'debounce'
+]);
---
+++ deepsense-navigation-panel/src/ui-widgets/deepsense-interaction-toolbar.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2015, CodiLime Inc.
+ *
+ * Created by: Grzegorz Swatowski
+ */
+
+(function() {
+  'use strict';
+
+  const DEFAULT_ID = 'unsetID';
+
+  /*@ngInject*/
+  function DeepsenseInteractionToolbar($rootScope) {
+    return {
+      restrict: 'E',
+      templateUrl: 'ui-widgets/deepsense-interaction-toolbar.html',
+      scope: {
+        'zoomId': '@',
+        'closeInnerWorkflowBtnVisible': '=?'
+      },
+      link: (scope) => {
+        scope.zoomId = scope.zoomId || DEFAULT_ID;
+        scope.$applyAsync(() => {
+          scope.handleZoomOutBtnClick = () => {
+            $rootScope.$broadcast('INTERACTION-PANEL.ZOOM-OUT', {
+              zoomId: scope.zoomId
+            });
+          };
+
+          scope.handleZoomInBtnClick = () => {
+            $rootScope.$broadcast('INTERACTION-PANEL.ZOOM-IN', {
+              zoomId: scope.zoomId
+            });
+          };
+
+          scope.handleCloseInnerWorkflowBtn = () => {
+            $rootScope.$broadcast('INTERACTION-PANEL.CLOSE-INNER-WORKFLOW');
+          };
+
+          scope.activeMoveBtn = false;
+          scope.handleMoveBtnClick = () => {
+            scope.activeMoveBtn = !scope.activeMoveBtn;
+            $rootScope.$broadcast('INTERACTION-PANEL.MOVE-GRAB', {
+              zoomId: scope.zoomId,
+              active: scope.activeMoveBtn
+            });
+          };
+
+          scope.handleFitBtnClick = () => {
+            $rootScope.$broadcast('INTERACTION-PANEL.FIT', {
+              zoomId: scope.zoomId
+            });
+          };
+        });
+      }
+    };
+  }
+
+  angular.module('deepsense.navigation-panel').
+    directive('deepsenseInteractionToolbar', DeepsenseInteractionToolbar);
+})();
