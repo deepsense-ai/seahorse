c635dd30861e623fd73d39f6a997723131c3607f
Michał Szostek <michal.szostek@deepsense.io>
2017-09-15 00:00:00 +0000

    Get rid of MaxPermSize flag
    This flag is ignored by Java 8.
    
    Change-Id: Ib0d5f7295ed48355c07558794f81d5681dc403d5

--- jenkins/workflow-executor_build_and_publish.sh
+++ jenkins/workflow-executor_build_and_publish.sh
@@ -14,11 +14,11 @@ source ../jenkins/publish_to_artifactory_function.sh
 # Build and publish to artifactory workflowexecutor.jar for Spark 1.6.1 and Scala 2.10 ONLY
 SPARK_VERSION=1.6.1
 SCALA_VERSION=2.10
-SBT_OPTS="-XX:MaxPermSize=4G" sbt -DsparkVersion=$SPARK_VERSION -Dsbt.log.noformat=true clean workflowexecutor/assembly
+sbt -DsparkVersion=$SPARK_VERSION -Dsbt.log.noformat=true clean workflowexecutor/assembly
 publish_to_artifactory target/workflowexecutor.jar seahorse-distribution/io/deepsense/$SEAHORSE_BUILD_TAG/workflowexecutor/workflowexecutor-$SCALA_VERSION-$SPARK_VERSION.jar
 
 # Build and publish to artifactory workflowexecutor.jar for Spark 2.0.0 and Scala 2.11 ONLY
 SPARK_VERSION=2.0.0
 SCALA_VERSION=2.11
-SBT_OPTS="-XX:MaxPermSize=4G" sbt -DsparkVersion=$SPARK_VERSION -Dsbt.log.noformat=true clean workflowexecutor/assembly
+sbt -DsparkVersion=$SPARK_VERSION -Dsbt.log.noformat=true clean workflowexecutor/assembly
 publish_to_artifactory target/workflowexecutor.jar seahorse-distribution/io/deepsense/$SEAHORSE_BUILD_TAG/workflowexecutor/workflowexecutor-$SCALA_VERSION-$SPARK_VERSION.jar
