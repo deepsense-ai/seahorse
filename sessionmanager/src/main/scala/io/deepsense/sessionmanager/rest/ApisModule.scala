3e8b5ae75add75553b6bbc5632e6154061aee129
Wojciech Jurczyk <wojciech.jurczyk@codilime.com>
2017-09-15 00:00:00 +0000

    Add Version API to Session Manager
    The commit adds support for "/version" url that shows
    information about the build.
    
    Change-Id: Id236005bbd7db29a3238ebb947865315ea61a882

--- sessionmanager/src/main/scala/io/deepsense/sessionmanager/rest/ApisModule.scala
+++ sessionmanager/src/main/scala/io/deepsense/sessionmanager/rest/ApisModule.scala
@@ -7,8 +7,10 @@ package io.deepsense.sessionmanager.rest
 import com.google.inject.AbstractModule
 import com.google.inject.binder.LinkedBindingBuilder
 import com.google.inject.multibindings.Multibinder
+import com.google.inject.name.Names._
 
-import io.deepsense.commons.rest.RestComponent
+import io.deepsense.commons.auth.AuthModule
+import io.deepsense.commons.rest.{RestComponent, VersionApi}
 
 class ApisModule extends AbstractModule {
   private lazy val apiBinder = Multibinder.newSetBinder(binder(), classOf[RestComponent])
@@ -18,6 +20,9 @@ class ApisModule extends AbstractModule {
   }
 
   override def configure(): Unit = {
+    bind(classOf[String]).annotatedWith(named("componentName")).toInstance("sessionmanager")
+    install(new AuthModule(useMockSecurity = true))
     bindApi.to(classOf[SessionsApi])
+    bindApi.to(classOf[VersionApi])
   }
 }
