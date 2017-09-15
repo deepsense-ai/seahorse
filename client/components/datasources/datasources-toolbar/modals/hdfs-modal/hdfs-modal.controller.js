91eb0d235e4e494f498b5b76fbabf5fdbeb5262a
Piotr Więckowski <piotr.wieckowski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-907 HDFS Modal
      * Adding new HDFS Datasource is possible
    
    Change-Id: Ic5a7728644e2f6f28ebd64b6b62caecadb185f67

--- client/components/datasources/datasources-toolbar/modals/hdfs-modal/hdfs-modal.controller.js
+++ client/components/datasources/datasources-toolbar/modals/hdfs-modal/hdfs-modal.controller.js
@@ -3,12 +3,60 @@
 import footerTpl from '../modal-footer/modal-footer.html';
 
 class HdfsModalController {
-  constructor($uibModalInstance) {
+  constructor($scope, $log, $uibModalInstance, datasourcesService) {
     'ngInject';
 
-    this.footerTpl = footerTpl;
+    _.assign(this, {$log, $uibModalInstance, datasourcesService});
 
-    _.assign(this, {$uibModalInstance});
+    this.footerTpl = footerTpl;
+    this.extension = 'csv';
+
+    this.datasourceParams = {
+      name: '',
+      visibility: 'privateVisibility',
+      datasourceType: 'hdfs',
+      hdfsParams: {
+        hdfsPath: '',
+        fileFormat: 'csv',
+        csvFileFormatParams: {
+          includeHeader: false,
+          convert01ToBoolean: false,
+          separatorType: '',
+          customSeparator: ''
+        }
+      }
+    };
+
+    $scope.$watch(() => this.datasourceParams, (newSettings) => {
+      this.datasourceParams = newSettings;
+      this.canAddNewDatasource = this.checkCanAddNewDatasource();
+    }, true);
+  }
+
+  checkCanAddNewDatasource() {
+    const isSeparatorValid = this.checkIsSeparatorValid();
+    const isSourceValid = this.datasourceParams.hdfsParams.hdfsPath !== '';
+    const isNameValid = this.datasourceParams.name !== '';
+
+    return isSeparatorValid && isSourceValid && isNameValid;
+  }
+
+  checkIsSeparatorValid() {
+    const {separatorType, customSeparator} = this.datasourceParams.hdfsParams.csvFileFormatParams;
+
+    if (separatorType) {
+      if (separatorType === 'custom') {
+        return customSeparator !== '';
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  onFileSettingsChange(data) {
+    this.datasourceParams.hdfsParams = Object.assign({}, this.datasourceParams.hdfsParams, data);
   }
 
   cancel() {
@@ -16,7 +64,14 @@ class HdfsModalController {
   }
 
   ok() {
-    this.$uibModalInstance.close();
+    this.datasourcesService.addDatasource(this.datasourceParams)
+      .then((result) => {
+        this.$log.info('result ', result);
+        this.$uibModalInstance.close();
+      })
+      .catch((error) => {
+        this.$log.info('error ', error);
+      });
   }
 }
 
