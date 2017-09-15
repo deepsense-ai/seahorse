2772e5ec2cdd9d80dac6aea399a620db219c81e9
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Remove exception package
    This change will simplify splitting spliting
    packages 'deeplang' and 'graphlib' to separate projects.
    
    Change-Id: Idad7724cf7c8ef3b834d3a01790205cdd95ac4b3

--- GraphLibrary/src/main/scala/io/deepsense/deeplang/exceptions/DeepLangException.scala
+++ GraphLibrary/src/main/scala/io/deepsense/deeplang/exceptions/DeepLangException.scala
@@ -6,6 +6,7 @@
 
 package io.deepsense.deeplang.exceptions
 
-import io.deepsense.exceptions.DeepSenseException
-
-abstract class DeepLangException(message: String) extends DeepSenseException(message)
+abstract class DeepLangException(
+    val message: String,
+    val cause: Throwable = null)
+  extends Exception(message, cause)
