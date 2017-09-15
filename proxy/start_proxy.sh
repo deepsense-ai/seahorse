e1819f3d73760c79f58b27e079285a4076bd7c6b
Michał Szostek <michal.szostek@deepsense.io>
2017-09-15 00:00:00 +0000

    Disable trial
    Change-Id: Ie4779bd6860579d1be88af2a6f36c42e569f037f

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-CREATION_DATE_FILE=/trial/.seahorse_creation_time
+#CREATION_DATE_FILE=/trial/.seahorse_creation_time
 
-cat $CREATION_DATE_FILE
+#cat $CREATION_DATE_FILE
 
-if [ ! -f $CREATION_DATE_FILE ]; then
-    echo "Proxy initialization"
-    mkdir -p /trial
-    date +%Y-%m-%d > $CREATION_DATE_FILE
-fi
+#if [ ! -f $CREATION_DATE_FILE ]; then
+#    echo "Proxy initialization"
+#    mkdir -p /trial
+#    date +%Y-%m-%d > $CREATION_DATE_FILE
+#fi
 
-export SEAHORSE_CREATION_DATE=`cat $CREATION_DATE_FILE`
+#export SEAHORSE_CREATION_DATE=`cat $CREATION_DATE_FILE`
 
 npm start
