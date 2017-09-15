534aedba8b22d7e4c4e2f640da7098ff578affd8
Piotr Misiak <piotr.misiak@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1774 move Workflow Manager from 9080 to 443 port
    Change-Id: Ic9f982630da0a08614ab33702599b76416eb5ba9
    (cherry picked from commit 9b2fa97386ab05e5803b9a2120223acbffb6966e)

--- deployment/ansible/roles/frontend/files/config.js
+++ deployment/ansible/roles/frontend/files/config.js
@@ -1,6 +1,5 @@
 angular.module('ds.lab').constant('config', {
   'apiHost': '${PUBLIC_WM_ADDRESS}',
-  'apiPort': '${PUBLIC_WM_PORT}',
   'apiVersion': '${API_VERSION}',
   'docsHost': '${PUBLIC_DOCS_ADDRESS}',
   'editorVersion': '${EDITOR_VERSION}',
