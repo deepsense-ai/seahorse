486ee59f79b577cca6514e5e058c149732cdefa2
Jaroslaw Osmanski <jaroslaw.osmanski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-452 Add ipam for default dockercompose_network to prepare_docker-compose
    Change-Id: Icd9cf47431400d1925e1cd867b4c339bcb53c9d2

--- jenkins/e2e_tests.sh
+++ jenkins/e2e_tests.sh
@@ -4,7 +4,10 @@
 
 
 (cd deployment/docker-compose ; ./docker-compose $SEAHORSE_BUILD_TAG pull)
+# destroy dockercompose_default, so we can recreate it with proper id
+(cd deployment/docker-compose ; ./docker-compose $SEAHORSE_BUILD_TAG down)
 (cd deployment/docker-compose ; ./docker-compose $SEAHORSE_BUILD_TAG up -d)
+
 
 sbt e2etests/clean e2etests/test
 SBT_EXIT_CODE=$?
