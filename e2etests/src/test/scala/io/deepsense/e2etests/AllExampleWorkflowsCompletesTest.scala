0717430e58ede4ada6b5220795c8a06c05a88145
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-158 Add e2e tests working on running instance of Seahorse
    * Tests run all example workflows and check if all nodes completed successfully
    * Tests do not assume anything about Seahorse state.
    
    Cherry-picked from
    	772b4615538d053afe86dc4c5773ebcdca3ce814
    	09e946eade84865913bf38b80fcfcbb10784f677
    
    Additionally
    * introduce wrapper script ./docker-compose to have same interface on all branches
    * disable oauth in dev docker-compose
    * change mq credentials
    
    Change-Id: I8ce79fba2fe6ff7d8d5beedde9398b2d0e351f6f

---
+++ e2etests/src/test/scala/io/deepsense/e2etests/AllExampleWorkflowsCompletesTest.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016, CodiLime Inc.
+ */
+
+package io.deepsense.e2etests
+
+import org.scalatest.{Matchers, WordSpec}
+
+class AllExampleWorkflowsCompletesTest extends WordSpec with Matchers with
+  SeahorseIntegrationTestDSL {
+
+  info("Assuming application will be accessible under localhost:8080")
+
+  "All examples workflows" should {
+    "be correct - all nodes run and completed successfully" in {
+
+      ensureSeahorseIsRunning()
+      val exampleWorkflowIds = getExampleWorkflowsIds()
+      exampleWorkflowIds should not be empty
+      exampleWorkflowIds.foreach { exampleWorkflowId =>
+        val clonedWorkflowId = cloneWorkflow(exampleWorkflowId)
+
+        withExecutor(clonedWorkflowId) { implicit ctx =>
+          launch(clonedWorkflowId)
+          assertAllNodesCompletedSuccessfully(clonedWorkflowId)
+        }
+
+        deleteWorkflow(clonedWorkflowId)
+      }
+    }
+  }
+
+}
