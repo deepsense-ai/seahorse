d41327d6ae037bad75b69de83b52c430734567f7
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1490 Change backend deployment
    Change-Id: Ib84309e05964423574e7164e43d80ab3d5618238

--- publish.sh
+++ publish.sh
@@ -6,23 +6,49 @@
 # This script expects no external parameters.
 # Version is calculated from current git sha, current time and BASE_VERSION variable
 
-BASE_VERSION="0.4.0"
+BASE_VERSION=`cat version.sbt | cut -d'=' -f 2 | xargs`
+SNAPSHOT_REPOSITORY="deepsense-backend-snapshot"
+RELEASE_REPOSITORY="deepsense-backend-release"
+
+function calculate_is_snapshot_version() {
+  if [[ "$BASE_VERSION" == *SNAPSHOT ]]
+  then
+    export IS_SNAPSHOT=true
+  else
+    export IS_SNAPSHOT=false
+  fi
+}
+
+function calculate_repository_url() {
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    export REPOSITORY_URL="$ARTIFACTORY_URL/$SNAPSHOT_REPOSITORY/io/deepsense"
+  else
+    export REPOSITORY_URL="$ARTIFACTORY_URL/$RELEASE_REPOSITORY/io/deepsense"
+  fi
+}
 
 function calculate_full_version() {
   echo "** Calculating version **"
-  DATE=`date -u +%Y-%m-%d_%H-%M-%S`
-  GIT_SHA=`git rev-parse HEAD`
-  GIT_SHA_PREFIX=${GIT_SHA:0:7}
-  export FULL_VERSION="${BASE_VERSION}-${DATE}-${GIT_SHA_PREFIX}-SNAPSHOT"
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    DATE=`date -u +%Y-%m-%d_%H-%M-%S`
+    GIT_SHA=`git rev-parse HEAD`
+    GIT_SHA_PREFIX=${GIT_SHA:0:7}    
+    export FULL_VERSION="${BASE_VERSION}-${DATE}-${GIT_SHA_PREFIX}"
+  else
+    export FULL_VERSION=${BASE_VERSION}
+  fi
 }
 
 function publish() {
   component=$1
-  artifactLocalName="${component}/target/universal/deepsense-${component}-${BASE_VERSION}-SNAPSHOT.zip"
+  artifactLocalName="${component}/target/universal/deepsense-${component}-${BASE_VERSION}.zip"
   artifactVersion=$2
+  url=$3
   artifactRemoteName="deepsense-${component}-${artifactVersion}.zip"
 
-  echo "** INFO: Uploading $artifactLocalName to $ARTIFACTORY_URL/deepsense-${component}/${artifactVersion} **"
+  echo "** INFO: Uploading $artifactLocalName to ${url} **"
   md5Value="`md5sum "${artifactLocalName}"`"
   md5Value="${md5Value:0:32}"
   sha1Value="`sha1sum "${artifactLocalName}"`"
@@ -32,16 +58,40 @@ function publish() {
    -H "X-Checksum-Md5: $md5Value" \
    -H "X-Checksum-Sha1: $sha1Value" \
    -T "${artifactLocalName}" \
-   "${ARTIFACTORY_URL}/deepsense-${component}/${artifactVersion}/${artifactRemoteName}"
+   "${url}"
+}
+
+function publishLatest() {
+  component=$1
+  artifactVersion="latest"
+  artifactRemoteName="deepsense-${component}-${artifactVersion}.zip"
+
+  publish $component $artifactVersion "${REPOSITORY_URL}/deepsense-${component}/deepsense-${component}-${artifactVersion}/${artifactRemoteName}"
+}
+
+function publishVersion() {
+  component=$1
+  artifactVersion=$2
+  artifactRemoteName="deepsense-${component}-${artifactVersion}.zip"
+
+  if [[ $IS_SNAPSHOT == true ]]
+  then
+    url="${REPOSITORY_URL}/deepsense-${component}/$BASE_VERSION/${artifactVersion}/${artifactRemoteName}"
+  else
+    url="${REPOSITORY_URL}/deepsense-${component}/${artifactVersion}/${artifactRemoteName}"
+  fi
+  publish $component $artifactVersion $url
 }
 
 function publish_component() {
-  publish $1 "${FULL_VERSION}"
-  publish $1 "latest"
+  publishVersion $1 "${FULL_VERSION}"
+  publishLatest $1
 }
 
+calculate_is_snapshot_version
+calculate_repository_url
 calculate_full_version
-for component in entitystorage workflowmanager graphexecutor deploy-model-service
+for component in entitystorage workflowmanager deploy-model-service
 do
   publish_component ${component}
 done
