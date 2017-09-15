36942214a64f39d41d7a2931f1e1a69083c65359
Marcin Kierski <marcin.kierski@codilime.com>
2017-09-15 00:00:00 +0000

    Fixing dependencies.
    Change-Id: I29d8f7d9dca30611dadabe2ca4bdff3678e351ca

--- deepsense-frontend/run_tests.sh
+++ deepsense-frontend/run_tests.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # This script is used by our CI (Jenkins) to run unit tests job.
-npm set registry http://10.10.1.124:4873
 npm install
 gulp build --ci
 npm run test -- --single-run
