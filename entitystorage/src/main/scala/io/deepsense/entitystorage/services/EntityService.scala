e25487108e9ebb2e36880dbf974b09a6fe38534e
Piotr Reszke <piotr.reszke@codilime.com>
2017-09-15 00:00:00 +0000

    Add logging for EntityService
    Change-Id: Ie54a993e344bb32bd21e23dad5c503c401c59e9e

--- entitystorage/src/main/scala/io/deepsense/entitystorage/services/EntityService.scala
+++ entitystorage/src/main/scala/io/deepsense/entitystorage/services/EntityService.scala
@@ -11,32 +11,42 @@ import scala.concurrent.{ExecutionContext, Future}
 import com.google.inject.Inject
 
 import io.deepsense.commons.datetime.DateTimeConverter
+import io.deepsense.commons.utils.Logging
 import io.deepsense.entitystorage.storage.EntityDao
 import io.deepsense.models.entities.{Entity, InputEntity, UserEntityDescriptor}
 
-class EntityService @Inject() (entityDao: EntityDao)(implicit ec: ExecutionContext) {
+class EntityService @Inject() (entityDao: EntityDao)(implicit ec: ExecutionContext)
+  extends Logging {
 
   /**
    * Returns all entities of tenant.
    */
-  def getAll(tenantId: String): Future[List[Entity]] = entityDao.getAll(tenantId)
+  def getAll(tenantId: String): Future[List[Entity]] = {
+    logger.debug("GetAll for tenantId: {}", tenantId)
+    entityDao.getAll(tenantId)
+  }
 
   /**
    * Returns reference to a physical storage where data associated with the entity are stored.
    */
-  def getEntityData(tenantId: String, entityId: Entity.Id): Future[Option[Entity]] =
+  def getEntityData(tenantId: String, entityId: Entity.Id): Future[Option[Entity]] = {
+    logger.debug("GetEntityData for tenantId {} and entityId: {}", tenantId, entityId)
     entityDao.get(tenantId, entityId).map(_.map(_.dataOnly))
+  }
 
   /**
    * Returns report associated with the entity.
    */
-  def getEntityReport(tenantId: String, entityId: Entity.Id): Future[Option[Entity]] =
+  def getEntityReport(tenantId: String, entityId: Entity.Id): Future[Option[Entity]] = {
+    logger.debug("GetEntityReport for tenantId: {} and entityId: {}", tenantId, entityId)
     entityDao.get(tenantId, entityId).map(_.map(_.reportOnly))
+  }
 
   /**
    * Creates new entity using input params.
    */
   def createEntity(inputEntity: InputEntity): Future[Entity] = {
+    logger.debug("CreateEntity for inputEntity: {}", inputEntity)
     val now = DateTimeConverter.now
     val entity = Entity(
       inputEntity.tenantId,
@@ -57,6 +67,7 @@ class EntityService @Inject() (entityDao: EntityDao)(implicit ec: ExecutionConte
    * Returns entity with Report if updated entity exists and None otherwise.
    */
   def updateEntity(tenantId: String, userEntity: UserEntityDescriptor): Future[Option[Entity]] = {
+    logger.debug("UpdateEntity for tenantId: {} and userEntity: {}", tenantId, userEntity)
     entityDao.get(tenantId, userEntity.id).flatMap {
       case Some(oldEntity) =>
         val newEntity = oldEntity.updateWith(userEntity)
@@ -69,6 +80,7 @@ class EntityService @Inject() (entityDao: EntityDao)(implicit ec: ExecutionConte
    * Removes entity pointed by id or does nothing if it does not exist.
    */
   def deleteEntity(tenantId: String, entityId: Entity.Id): Future[Unit] = {
+    logger.debug("DeleteEntity for tenantId: {} and entityId: {}", tenantId, entityId)
     entityDao.delete(tenantId, entityId)
   }
 }
