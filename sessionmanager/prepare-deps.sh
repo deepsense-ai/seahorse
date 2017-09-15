4e00a241fbd87c74a100036f58a7b75b00ee5c65
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Fix paths so it works on jenkins
    * Jenkins have spaces in paths - those made variables being split
    
    Change-Id: I10893678e760f4a8d09c78eb99d8b2ec09bffd28

--- sessionmanager/prepare-deps.sh
+++ sessionmanager/prepare-deps.sh
@@ -6,21 +6,19 @@ if [ "$#" != "1" ] ; then
 fi
 
 PROJECT=`readlink -f $1` # absolute path
-echo "Project dir: $PROJECT"
-pwd
-SM_DIR=$PROJECT/sessionmanager
+SM_DIR="$PROJECT/sessionmanager"
 
-TMPDIR=$SM_DIR/target/_we_deps_tmp
+TMPDIR="$SM_DIR/target/_we_deps_tmp"
 
 rm -Rf $TMPDIR
 mkdir $TMPDIR
 
-WE_DEPS_CONTENT=$TMPDIR/we_deps_target
+WE_DEPS_CONTENT="$TMPDIR/we_deps_target"
 mkdir $WE_DEPS_CONTENT
 
 echo "Copying pyexecutor from workflowexecutor jar"
 
-WE_JAR=$SM_DIR/target/downloads/we.jar
+WE_JAR="$SM_DIR/target/downloads/we.jar"
 (cd $WE_DEPS_CONTENT; jar xf $WE_JAR pyexecutor)
 
 echo "Copying executing kernel"
@@ -31,7 +29,7 @@ unzip $PROJECT/remote_notebook/notebook_executing_kernel.zip -d $WE_DEPS_CONTENT
 echo "Copying files from we-deps-consts files"
 cp -R $SM_DIR/we-deps-consts/* $WE_DEPS_CONTENT
 
-WE_DEPS_TARGET=$SM_DIR/target/we-deps.zip
+WE_DEPS_TARGET="$SM_DIR/target/we-deps.zip"
 rm -f $WE_DEPS_TARGET
 (cd $WE_DEPS_CONTENT; zip -r we-deps.zip *)
 mv $WE_DEPS_CONTENT/we-deps.zip $WE_DEPS_TARGET
