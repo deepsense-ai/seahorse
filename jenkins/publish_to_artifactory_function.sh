1227a5f31fc38926baff16cd57338dc108d4ad73
Grzegorz Chilkiewicz <grzegorz.chilkiewicz@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-562 Use publish_to_artifactory function in jenkins scripts
    Change-Id: Ic5c4ad6d9bf8ef32787db81b52f84e73c9a4aa33

---
+++ jenkins/publish_to_artifactory_function.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -ex
+# Copyright (c) 2016, CodiLime Inc.
+#
+# Function for publishing artifacts to deepsense.io internal artifactory
+#
+# Usage:
+# source jenkins/publish_to_artifactory_function.sh
+# publish_to_artifactory jenkins/test test-local/io/deepsense/testing
+
+function publish_to_artifactory {
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: publish PATH_TO_FILE PATH_IN_ARTIFACTORY"
+    echo "This will publish \$PATH_TO_FILE to http://artifactory.deepsense.codilime.com:8081/artifactory/\$PATH_IN_ARTIFACTORY"
+    exit 1
+  fi
+
+  local ARTIFACTORY_CREDENTIALS=$HOME/.artifactory_credentials
+  local ARTIFACTORY_USER=`grep "user=" $ARTIFACTORY_CREDENTIALS | cut -d '=' -f 2`
+  local ARTIFACTORY_PASSWORD=`grep "password=" $ARTIFACTORY_CREDENTIALS | cut -d '=' -f 2`
+  local ARTIFACTORY_URL=`grep "host=" $ARTIFACTORY_CREDENTIALS | cut -d '=' -f 2`
+
+  local LOCAL_ARTIFACT_LOCATION=$1
+  local REMOTE_ARTIFACT_LOCATION=$ARTIFACTORY_URL/$2
+
+  echo "** INFO: Uploading $LOCAL_ARTIFACT_LOCATION to ${REMOTE_ARTIFACT_LOCATION} **"
+  local md5Value="`md5sum "${LOCAL_ARTIFACT_LOCATION}"`"
+  local md5Value="${md5Value:0:32}"
+  local sha1Value="`sha1sum "${LOCAL_ARTIFACT_LOCATION}"`"
+  local sha1Value="${sha1Value:0:40}"
+
+  curl -i -X PUT -u $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD \
+   -H "X-Checksum-Md5: $md5Value" \
+   -H "X-Checksum-Sha1: $sha1Value" \
+   -T "${LOCAL_ARTIFACT_LOCATION}" \
+   "${REMOTE_ARTIFACT_LOCATION}"
+}
--- jenkins/workflowexecutor-publish.sh
+++ jenkins/workflowexecutor-publish.sh
@@ -26,21 +26,15 @@ RELEASE_REPOSITORY="seahorse-workflowexecutor-release"
 SEAHORSE_WORKFLOWEXECUTOR_REPOSITORY="seahorse-workflowexecutor"
 
 
+# Functions useful for publishing artifacts
+source jenkins/publish_to_artifactory_function.sh
+
 function calculate_is_snapshot_version() {
   if [[ "$BASE_VERSION" == *SNAPSHOT ]]
   then
     export IS_SNAPSHOT=true
   else
     export IS_SNAPSHOT=false
-  fi
-}
-
-function calculate_repository_url() {
-  if [[ $IS_SNAPSHOT == true ]]
-  then
-    export REPOSITORY_URL="$ARTIFACTORY_URL/$SNAPSHOT_REPOSITORY/io/deepsense"
-  else
-    export REPOSITORY_URL="$ARTIFACTORY_URL/$RELEASE_REPOSITORY/io/deepsense"
   fi
 }
 
@@ -57,34 +51,13 @@ function calculate_full_version() {
   fi
 }
 
-function publish() {
-  component=$1
-  artifactVersion=$2
-  # artifactLocalName should be like "${component}/target/scala-${scalaVersion}/${component}.jar"
-  artifactLocalName=$3
-  url=$4
-
-
-  echo "** INFO: Uploading $artifactLocalName to ${url} **"
-  md5Value="`md5sum "${artifactLocalName}"`"
-  md5Value="${md5Value:0:32}"
-  sha1Value="`sha1sum "${artifactLocalName}"`"
-  sha1Value="${sha1Value:0:40}"
-
-  curl -i -X PUT -u $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD \
-   -H "X-Checksum-Md5: $md5Value" \
-   -H "X-Checksum-Sha1: $sha1Value" \
-   -T "${artifactLocalName}" \
-   "${url}"
-}
-
 function publish_custom() {
   component=$1
   scalaVersion=$2
   artifactVersion=$3
   artifactRemoteName="${component}_${scalaVersion}-${artifactVersion}.jar"
 
-  publish $component $artifactVersion "${component}/target/scala-${scalaVersion}/${component}.jar" "${REPOSITORY_URL}/${component}_${scalaVersion}/${artifactVersion}/${artifactRemoteName}"
+  publish_to_artifactory "${component}/target/scala-${scalaVersion}/${component}.jar" "${component}_${scalaVersion}/${artifactVersion}/${artifactRemoteName}"
 }
 
 function publish_latest() {
@@ -99,11 +72,11 @@ function publish_version() {
 
   if [[ $IS_SNAPSHOT == true ]]
   then
-    url="${REPOSITORY_URL}/${component}_${scalaVersion}/$BASE_VERSION/${artifactVersion}/${artifactRemoteName}"
+    url="${component}_${scalaVersion}/$BASE_VERSION/${artifactVersion}/${artifactRemoteName}"
   else
-    url="${REPOSITORY_URL}/${component}_${scalaVersion}/${artifactVersion}/${artifactRemoteName}"
+    url="${component}_${scalaVersion}/${artifactVersion}/${artifactRemoteName}"
   fi
-  publish $component $artifactVersion "${component}/target/scala-${scalaVersion}/${component}.jar" $url
+  publish_to_artifactory "${component}/target/scala-${scalaVersion}/${component}.jar" $url
 }
 
 function publish_scala() {
@@ -121,47 +94,19 @@ function publish_component() {
   fi
 }
 
-function prepare_system_tests_propfile() {
 
-  PROPFILE_NAME=latestpropfile
-
-  if [ $GIT_BRANCH != "origin/master" ]
-  then
-      PROPFILE_NAME=productionpropfile
-  fi
-
-  SYSTEM_TESTS_VERSION=latest
-  WORKFLOW_EXECUTOR_SOURCE=seahorse-workflowexecutor-snapshot
-  WORKFLOW_EXECUTOR_VERSION=${BASE_VERSION}-latest
-
-  if [ -f latestpropfile ];
-  then
-      rm latestpropfile
-  fi
-
-  if [ -f productionpropfile ];
-  then
-      rm productionpropfile
-  fi
-
-  echo SYSTEM_TESTS_VERSION=$(echo $SYSTEM_TESTS_VERSION) >> $PROPFILE_NAME
-  echo WORKFLOW_EXECUTOR_SOURCE=$(echo $WORKFLOW_EXECUTOR_SOURCE) >> $PROPFILE_NAME
-  echo WORKFLOW_EXECUTOR_VERSION=$(echo $WORKFLOW_EXECUTOR_VERSION) >> $PROPFILE_NAME
-}
-
-
+# Publish projects jars
 calculate_is_snapshot_version
 calculate_full_version
-calculate_repository_url
 publish_component workflowexecutor
-prepare_system_tests_propfile
 
+
+# Publish WE uber-jar
 if [ "$SEAHORSE_BUILD_TAG" != "" ]; then
   scalaVersions=("2.11")
   for scalaVersion in "${scalaVersions[@]}"
   do
-    publish "workflowexecutor" $SEAHORSE_BUILD_TAG "workflowexecutor/target/scala-${scalaVersion}/workflowexecutor.jar" \
-      "${ARTIFACTORY_URL}/${SEAHORSE_WORKFLOWEXECUTOR_REPOSITORY}/io/deepsense/deepsense-seahorse-workflowexecutor_${scalaVersion}/${SEAHORSE_BUILD_TAG}/workflowexecutor.jar"
+    publish_to_artifactory "workflowexecutor/target/scala-${scalaVersion}/workflowexecutor.jar" \
+      "${SEAHORSE_WORKFLOWEXECUTOR_REPOSITORY}/io/deepsense/deepsense-seahorse-workflowexecutor_${scalaVersion}/${SEAHORSE_BUILD_TAG}/workflowexecutor.jar"
   done
-
 fi
