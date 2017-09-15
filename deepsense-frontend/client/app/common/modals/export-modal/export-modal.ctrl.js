a4a8595fbbecf18039ff786833e99d868e57e413
Jakub Sadłowski <jakub.sadlowski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-392 Remove all obsolete links to documentation in Frontend
    Change-Id: I290021c7ea1f1aec44edb1fa0dbe472bfa78d0e0

--- deepsense-frontend/client/app/common/modals/export-modal/export-modal.ctrl.js
+++ deepsense-frontend/client/app/common/modals/export-modal/export-modal.ctrl.js
@@ -1,31 +1,16 @@
 'use strict';
 
 /* @ngInject */
-function ExportModalController($uibModalInstance, $stateParams, config, version, WorkflowsApiClient) {
+function ExportModalController($uibModalInstance, $stateParams, WorkflowsApiClient) {
 
   const vm = this;
 
   vm.downloadLink = WorkflowsApiClient.getDownloadWorkflowMethodUrl($stateParams.id);
 
   vm.close = close;
-  vm.getExecutorLink = getExecutorLink;
-  vm.getDocsHost = getDocsHost;
-  vm.getDocsVersion = getDocsVersion;
 
   function close() {
     $uibModalInstance.dismiss();
-  }
-
-  function getExecutorLink() {
-    return `https://s3.amazonaws.com/workflowexecutor/releases/${config.apiVersion}/workflowexecutor_2.10-${config.apiVersion}.jar`;
-  }
-
-  function getDocsHost() {
-    return config.docsHost;
-  }
-
-  function getDocsVersion() {
-    return version.getDocsVersion();
   }
 }
 
