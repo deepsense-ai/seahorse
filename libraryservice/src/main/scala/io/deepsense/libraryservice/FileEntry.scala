0eee09ac93c438530227394f393bb6c156623c66
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Remove uri for files in library
    Change-Id: I175f2320451c1527864ac0d64ce08484621e3858

--- libraryservice/src/main/scala/io/deepsense/libraryservice/FileEntry.scala
+++ libraryservice/src/main/scala/io/deepsense/libraryservice/FileEntry.scala
@@ -6,18 +6,8 @@ package io.deepsense.libraryservice
 
 import spray.json.DefaultJsonProtocol
 
-case class FileEntry(name: String, uri: String)
-
-object FileEntry {
-  def apply(name: String): FileEntry = new FileEntry(name, uriForName(name))
-
-  private val UriPrefix = "file://"
-
-  private def uriForName(name: String): String = {
-    UriPrefix + Config.Storage.directory + name
-  }
-}
+case class FileEntry(name: String)
 
 object FileEntryJsonProtocol extends DefaultJsonProtocol {
-  implicit val fileEntryJsonFormat = jsonFormat2(FileEntry.apply)
+  implicit val fileEntryJsonFormat = jsonFormat1(FileEntry.apply)
 }
