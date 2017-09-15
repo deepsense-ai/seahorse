c71963dc94041ec967c52570274c9ef085a20a15
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Fix R library paths
    Change-Id: I7b516e186ea12681bcfeb3a224e08aca4f4049aa

--- remote_notebook/code/sparkr_kernel/kernel_init.R
+++ remote_notebook/code/sparkr_kernel/kernel_init.R
@@ -1,7 +1,7 @@
 entryPointId <- "0"
 
 # R will install packages to first lib path in here. We will mount it as docker volume to persist packages.
-.libPaths(c(file.path("/opt/R_Libs"), c(file.path("/opt/spark-2.0.0/R/lib/"), .libPaths()))
+.libPaths(c(file.path("/opt/R_Libs"), file.path("/opt/spark-2.0.0/R/lib/"), .libPaths()))
 library(SparkR)
 
 SparkR:::connectBackend(r_backend_host, r_backend_port)
