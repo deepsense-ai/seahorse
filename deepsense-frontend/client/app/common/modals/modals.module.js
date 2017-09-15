2e2c5ace10d36141dbfb626442617f6cecb62e28
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Remove never used stuff from upload-execution-report era
    Cherry-picked from 79e9b51eec5344666761404baf8906d6fa849c76
    
    Change-Id: I9fcbadf3674724c76fd9706d440fde75739dc0bb

--- deepsense-frontend/client/app/common/modals/modals.module.js
+++ deepsense-frontend/client/app/common/modals/modals.module.js
@@ -3,7 +3,6 @@
 exports.inject = function(module) {
   require('./new-workflow-modal/new-workflow-modal.ctrl.js').inject(module);
   require('./upload-workflow-modal/upload-workflow-modal.ctrl.js').inject(module);
-  require('./upload-execution-report-modal/upload-execution-report-modal.ctrl.js').inject(module);
   require('./confirmation-modal/confirmation-modal.ctrl.js').inject(module);
   require('./confirmation-modal/confirmation-modal.service.js').inject(module);
   require('./export-modal/export-modal.ctrl.js').inject(module);
