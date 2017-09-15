258aba794e58335bf5abc3e7df1784feac21266c
Witold Jędrzejewski <witold.jedrzejewski@codilime.com>
2017-09-15 00:00:00 +0000

    Add error description to Node json protocol
    Change-Id: I1df1766e2ed73a445a4d9e7be74040c27ef4fb38

--- graphjson/src/main/scala/io/deepsense/graphjson/NodeStateJsonProtocol.scala
+++ graphjson/src/main/scala/io/deepsense/graphjson/NodeStateJsonProtocol.scala
@@ -7,10 +7,14 @@ package io.deepsense.graphjson
 import spray.json._
 
 import io.deepsense.commons.json.DateTimeJsonProtocol._
+import io.deepsense.commons.json.ExceptionsJsonProtocol
+import io.deepsense.graph.Status.Status
 import io.deepsense.graph.{Progress, State}
 
-trait NodeStateJsonProtocol extends DefaultJsonProtocol{
-
+trait NodeStateJsonProtocol
+  extends DefaultJsonProtocol
+  with ExceptionsJsonProtocol
+  with NullOptions {
   implicit val progressFormat = jsonFormat2(Progress.apply)
 
   implicit object NodeStateWriter
@@ -24,7 +28,8 @@ trait NodeStateJsonProtocol extends DefaultJsonProtocol{
         NodeStateJsonProtocol.Started -> state.started.toJson,
         NodeStateJsonProtocol.Ended -> state.ended.toJson,
         NodeStateJsonProtocol.Progress -> state.progress.toJson,
-        NodeStateJsonProtocol.Results -> state.results.map(_.map(_.toString)).toJson
+        NodeStateJsonProtocol.Results -> state.results.map(_.map(_.toString)).toJson,
+        NodeStateJsonProtocol.Error -> state.error.toJson
       )
     }
   }
@@ -36,4 +41,5 @@ object NodeStateJsonProtocol extends NodeStateJsonProtocol {
   val Ended = "ended"
   val Progress = "progress"
   val Results = "results"
+  val Error = "error"
 }
