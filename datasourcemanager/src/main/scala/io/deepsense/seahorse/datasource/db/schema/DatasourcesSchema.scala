5ea4c689a3eccdb6f9034f361477c329dbc42ea5
Adam Jakubowski <adam.jakubowski@codilime.com>
2017-09-15 00:00:00 +0000

    Comment out PrintDDL object breaking docker building
    Change-Id: I626b61b8d523557fee3258b98ee414e6a000c81c

--- datasourcemanager/src/main/scala/io/deepsense/seahorse/datasource/db/schema/DatasourcesSchema.scala
+++ datasourcemanager/src/main/scala/io/deepsense/seahorse/datasource/db/schema/DatasourcesSchema.scala
@@ -86,6 +86,7 @@ object DatasourcesSchema {
 // TODO use sbt-docker with sbt-assembly and define mainClass in assembly as
 // it's solved in Neptune
 
+/*
 object PrintDDL extends App {
   import Database.api._
   import DatasourcesSchema._
@@ -93,4 +94,4 @@ object PrintDDL extends App {
   println(datasourcesTable.schema.createStatements.mkString("\n"))
   // scalastyle:on println
 }
-
+*/
