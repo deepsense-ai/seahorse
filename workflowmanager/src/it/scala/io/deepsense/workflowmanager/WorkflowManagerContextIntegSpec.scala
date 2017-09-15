1bf95a928f7d14e488c605755d65467a001b334e
Wojciech Jurczyk <wojciech.jurczyk@codilime.com>
2017-09-15 00:00:00 +0000

    Fix ds-it by mixing missing CassandraTestSupport
    WorkflowManagerContextIntegSpec requires Cassandra running but the test
    did not start it. Now, the test mixins CassandraTestSupport and starts
    an embeded Cassandra.
    
    Change-Id: Id1c9ad8da8063f3e4258f5dc1299183b6b5b17f5

--- workflowmanager/src/it/scala/io/deepsense/workflowmanager/WorkflowManagerContextIntegSpec.scala
+++ workflowmanager/src/it/scala/io/deepsense/workflowmanager/WorkflowManagerContextIntegSpec.scala
@@ -5,20 +5,21 @@
 package io.deepsense.workflowmanager
 
 import com.google.inject.Guice
-import org.scalatest.FlatSpec
-
+import io.deepsense.commons.StandardSpec
+import io.deepsense.commons.cassandra.CassandraTestSupport
 import io.deepsense.deeplang.catalogs.doperable.DOperableCatalog
-import io.deepsense.workflowmanager.rest.InsecureWorkflowApi
-import io.deepsense.workflowmanager.rest.InsecureOperationsApi
+import io.deepsense.workflowmanager.rest.{InsecureOperationsApi, InsecureWorkflowApi}
 
-class WorkflowManagerContextIntegSpec extends FlatSpec {
+class WorkflowManagerContextIntegSpec extends StandardSpec with CassandraTestSupport {
 
   val injector = Guice.createInjector(new WorkflowManagerAppModule(withMockedSecurity = false))
 
-  "Guice context" should "have all needed services created" in {
-    checkIfSingleton(classOf[DOperableCatalog])
-    assert(injector.getInstance(classOf[InsecureOperationsApi]) != null)
-    assert(injector.getInstance(classOf[InsecureWorkflowApi]) != null)
+  "Guice context" should {
+    "have all needed services created" in {
+      checkIfSingleton(classOf[DOperableCatalog])
+      assert(injector.getInstance(classOf[InsecureOperationsApi]) != null)
+      assert(injector.getInstance(classOf[InsecureWorkflowApi]) != null)
+    }
   }
 
   private def checkIfSingleton[T <: AnyRef](clazz: Class[T]) = {
@@ -26,4 +27,8 @@ class WorkflowManagerContextIntegSpec extends FlatSpec {
     val instance2: T = injector.getInstance(clazz)
     assert(instance1 eq instance2)
   }
+
+  override def cassandraTableName: String = "foobar"
+
+  override def cassandraKeySpaceName: String = "workflowmanager"
 }
