fb64207350084448d80f93cbf6b08dc5818d6d7b
Wojciech Jurczyk <wojciech.jurczyk@codilime.com>
2017-09-15 00:00:00 +0000

    Create SBT application bootstrap
    Change-Id: Id5ffe9cf5d94ba333f2473d87c9eb95daa1897f2

---
+++ experimentmanager/build.sbt
@@ -0,0 +1,5 @@
+name := "Experiment Manger"
+
+version := "0.1.0"
+
+scalaVersion := "2.10.4"
---
+++ experimentmanager/src/main/scala/io/deepsense/experimentmanager/ExperimentManagerApp.scala
@@ -0,0 +1,9 @@
+package io.deepsense.experimentmanager
+
+/**
+ * This is the entry point of the Experiment Manager application.
+ * @author Wojciech Jurczyk (wojciech.jurczyk@codilime.com)
+ */
+object ExperimentManagerApp extends App {
+  override def main(args: Array[String]) = println("ExperimentManager is starting...")
+}
