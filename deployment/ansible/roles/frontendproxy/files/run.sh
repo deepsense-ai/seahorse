3c988dadc4d6e1eb9b340fecfc274896fd122b85
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    DS-2337 Remove frontend proxy from deployment code.
    Change-Id: Ibb9c592f7685457465f58285083fcdd55175c75f

--- deployment/ansible/roles/frontendproxy/files/run.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-# Copyright (c) 2015, CodiLime Inc.
-
-cd ..
-eval "echo \"$(<config.json)\"" > build/server/config.json
-cd build
-cat server/config.json
-npm run start >> frontend-proxy.out 2>> frontend-proxy.err
