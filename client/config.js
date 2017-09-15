16d4bb02d8aee120e529f21af8eb56bc298825f4
Konrad Szałwiński <konrad.szalwinski@codilime.com>
2017-09-15 00:00:00 +0000

    DS-1414 Application versioning procedure
    Change-Id: Ieb6de28700f9d1e63f65ff664944359600b722fa

--- client/config.js
+++ client/config.js
@@ -2,5 +2,6 @@ angular.module('ds.lab').constant('config', {
   'apiHost': 'http://52.88.28.65',
   'apiPort': '9080',
   'apiVersion': '0.9.0',
+  'editorVersion': '1.0.0',
   'urlApiVersion': 'v1'
 });
