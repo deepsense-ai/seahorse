a5d6ba2aff5140e55c78f8b639e8309469e3b809
Piotr Więckowski <piotr.wieckowski@codilime.com>
2017-09-15 00:00:00 +0000

    SHR-976 Google spreadsheet modal validation
    Change-Id: Idab1a38da61ff4ca1c624a261b34793700faa07f

--- client/components/datasources/datasources-toolbar/modals/google-spreadsheet-modal/google-spreadsheet-modal.controller.js
+++ client/components/datasources/datasources-toolbar/modals/google-spreadsheet-modal/google-spreadsheet-modal.controller.js
@@ -2,6 +2,9 @@
 
 import BaseDatasourceModalController from '../base-datasource-modal-controller.js';
 
+const GOOGLE_SPREADSHEET_REGEX = /[a-zA-Z0-9-_]+/;
+const FULL_SPREADSHEET_URI_REGEX = /\/spreadsheets\/d\/([a-zA-Z0-9-_]+)/;
+
 class GoogleSpreadsheetModalController extends BaseDatasourceModalController {
   constructor($scope, $log, $uibModalInstance, datasourcesService, editedDatasource) {
     'ngInject';
@@ -29,14 +32,35 @@ class GoogleSpreadsheetModalController extends BaseDatasourceModalController {
       this.datasourceParams = newSettings;
       this.canAddNewDatasource = this.canAddDatasource();
     }, true);
+
+    $scope.$watch(() => this.datasourceParams.googleSpreadsheetParams.googleSpreadsheetId, (newValue, oldValue) => {
+      if (newValue !== oldValue) {
+        const results = this.datasourceParams.googleSpreadsheetParams.googleSpreadsheetId.match(FULL_SPREADSHEET_URI_REGEX);
+        if (results && results.length) {
+          this.datasourceParams.googleSpreadsheetParams.googleSpreadsheetId = results[1];
+        }
+      }
+    });
   }
 
   canAddDatasource() {
-    return this.datasourceParams.googleSpreadsheetParams.googleSpreadsheetId !== '' &&
-      this.datasourceParams.googleSpreadsheetParams.googleServiceAccountCredentials !== '' &&
-      this.datasourceParams.name !== '' &&
-      !super.doesNameExists();
+    return this.datasourceParams.name !== '' && this.isGoogleSpreadsheetIdValid() &&
+      this.isGoogleCrendetialsValid() && !super.doesNameExists();
   }
+
+  isGoogleSpreadsheetIdValid() {
+    return !!this.datasourceParams.googleSpreadsheetParams.googleSpreadsheetId.match(GOOGLE_SPREADSHEET_REGEX);
+  }
+
+  isGoogleCrendetialsValid() {
+    try {
+      JSON.parse(this.datasourceParams.googleSpreadsheetParams.googleServiceAccountCredentials);
+    } catch(e) {
+      this.areCredentialsValid = false;
+    }
+    this.areCredentialsValid = true;
+  }
+
 }
 
 export default GoogleSpreadsheetModalController;
