4d9baf946bf1b1fa2d64177af73e73db7ccaed6f
Rafal Hryciuk <rafal.hryciuk@codilime.com>
2017-09-15 00:00:00 +0000

    Make WM fail when application context cannot be created.
    Change-Id: I89bd249cbe6a14ff186cd7c9f1617f19556b2db0

--- workflowmanager/src/main/scala/io/deepsense/workflowmanager/WorkflowManagerApp.scala
+++ workflowmanager/src/main/scala/io/deepsense/workflowmanager/WorkflowManagerApp.scala
@@ -8,6 +8,7 @@ import akka.actor.ActorSystem
 import com.google.inject.{Guice, Stage}
 
 import io.deepsense.commons.rest.RestServer
+import io.deepsense.commons.utils.Logging
 import io.deepsense.deeplang.CatalogRecorder
 import io.deepsense.deeplang.catalogs.doperable.DOperableCatalog
 import io.deepsense.deeplang.catalogs.doperations.DOperationsCatalog
@@ -15,14 +16,18 @@ import io.deepsense.deeplang.catalogs.doperations.DOperationsCatalog
 /**
  * This is the entry point of the Workflow Manager application.
  */
-object WorkflowManagerApp extends App {
-  val insecure: Boolean = args.headOption.map("insecure".equalsIgnoreCase(_)).getOrElse(true)
-  val injector = Guice.createInjector(Stage.PRODUCTION, new WorkflowManagerAppModule(insecure))
+object WorkflowManagerApp extends App with Logging {
+  val insecure: Boolean = args.headOption.map("insecure".equalsIgnoreCase).getOrElse(true)
 
-  CatalogRecorder.registerDOperables(injector.getInstance(classOf[DOperableCatalog]))
-  CatalogRecorder.registerDOperations(injector.getInstance(classOf[DOperationsCatalog]))
-
-  injector.getInstance(classOf[RestServer]).start()
-  injector.getInstance(classOf[ActorSystem]).awaitTermination()
-  System.exit(1)
+  try {
+    val injector = Guice.createInjector(Stage.PRODUCTION, new WorkflowManagerAppModule(insecure))
+    CatalogRecorder.registerDOperables(injector.getInstance(classOf[DOperableCatalog]))
+    CatalogRecorder.registerDOperations(injector.getInstance(classOf[DOperationsCatalog]))
+    injector.getInstance(classOf[RestServer]).start()
+    injector.getInstance(classOf[ActorSystem]).awaitTermination()
+  } catch {
+    case e: Exception =>
+      logger.error("Application context creation failed", e)
+      System.exit(1)
+  }
 }
