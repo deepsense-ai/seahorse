3305b25a48f4dd5859a1d719e8d371cc81ced49d
Jaroslaw Osmanski <jaroslaw.osmanski@codilime.com>
2018-03-07 15:59:32 +0100

    Restore test with newline and space before next row of cluster preset option

--- sessionmanager/src/test/scala/ai/deepsense/sessionmanager/service/sessionspawner/sparklauncher/clusters/SparkArgumentParserTest.scala
+++ sessionmanager/src/test/scala/ai/deepsense/sessionmanager/service/sessionspawner/sparklauncher/clusters/SparkArgumentParserTest.scala
@@ -32,6 +32,11 @@ class SparkArgumentParserTest extends FunSuite with Matchers {
     }
   }
 
+  val multipleConfValues = mutable.Set[String](
+    "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps",
+    "spark.driver.extraJavaOptions=-XX:+PrintGCDetails")
+  val mutipleConfOutput = Map("--conf" -> multipleConfValues)
+
   test("Parsing - multiple conf options") {
     val mulitpleConf =
       """
@@ -40,10 +45,15 @@ class SparkArgumentParserTest extends FunSuite with Matchers {
         |""".stripMargin
 
 
-    val multipleConfValues = mutable.Set[String](
-      "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps",
-      "spark.driver.extraJavaOptions=-XX:+PrintGCDetails")
-    val mutipleConfOutput = Map("--conf" -> multipleConfValues)
+    SparkArgumentParser.parse(mulitpleConf) shouldEqual mutipleConfOutput.success
+  }
+
+  test("Parsing - space after new line") {
+    val mulitpleConf =
+      """
+        |--conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps"
+        | --conf "spark.driver.extraJavaOptions=-XX:+PrintGCDetails"
+        |""".stripMargin
     SparkArgumentParser.parse(mulitpleConf) shouldEqual mutipleConfOutput.success
   }
 
